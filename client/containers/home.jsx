93ae11a230d419907204d214f0f8ed745af3ec40
Eric Shao <eric.shao@qeemo.me>
2016-01-19 21:18:44 +0800

    修改设计按钮

--- client/containers/home.jsx
+++ client/containers/home.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import {Button} from '../../reusable/ant-ui';
+import {Button, Icon} from '../../reusable/ant-ui';
 import AmNavBar from '../components/am-navbar';
 import NavLink from '../../reusable/components/nav-link';
 import ModuleLayout from '../components/module-layout';
@@ -29,8 +29,8 @@ export default class Home extends React.Component {
               <div className="tenant-logo " style={{backgroundImage:`url("${logo || '/assets/img/home/tenant-logo.png'}")`}} />
               <h2 className="tenant-name">{name}</h2>
             </div>
-            <div className="btn-group">
-              <NavLink to="/corp/info"><Button type="primary" size="large"><span>设置</span></Button></NavLink>
+            <div className="setting-btn-wrap">
+              <NavLink to="/corp/info"><Button type="primary" shape="circle-outline" size="large"><Icon type="search" /></Button></NavLink>
             </div>
             <div className="tenant-nav">
             </div>
