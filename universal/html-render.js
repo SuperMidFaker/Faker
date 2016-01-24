8b1599fbdafdeb930168964f38d3d39a64adfbf7
Eric Shao <eric.shao@qeemo.me>
2016-01-25 00:37:50 +0800

    modified head elements in html-render.js cleaned img folder

--- universal/html-render.js
+++ universal/html-render.js
@@ -14,14 +14,21 @@ function renderAsHtml(pageCss, pageJs, content) {
 <!DOCTYPE html>
 <html>
 <head>
+  <title>WeLogix</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <title>关云掌</title>
   <link rel="shortcut icon" href="${__CDN__}/assets/img/favicon.ico${tv}"/>
+  <link rel="apple-touch-icon-precomposed" href="${__CDN__}/assets/img/apple-touch-icon-57x57-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" href="${__CDN__}/assets/img/apple-touch-icon-72x72-precomposed.png" sizes="72x72" />
+  <link rel="apple-touch-icon-precomposed" href="${__CDN__}/assets/img/apple-touch-icon-114x114-precomposed.png" sizes="114x114" />
+  <link rel="apple-touch-icon-precomposed" href="${__CDN__}/assets/img/apple-touch-icon-144x144-precomposed.png" sizes="144x144" />
   <link rel="stylesheet" type="text/css" href="${__CDN__}/assets/lib/stroke-7/style.css" />
   <link rel="stylesheet" type="text/css" href="${__CDN__}/assets/lib/md-iconic-font/css/material-design-iconic-font.min.css">
   <link rel="stylesheet" type="text/css" href="${__CDN__}/assets/lib/jquery.nanoscroller/css/nanoscroller.css" />
   ${pageCss}
+  <!--[if lt IE 10]>
+    <script src="https://as.alipayobjects.com/g/component/??console-polyfill/0.2.2/index.js,es5-shim/4.1.14/es5-shim.min.js,es5-shim/4.1.14/es5-sham.min.js,html5shiv/3.7.2/html5shiv.min.js,media-match/2.0.2/media.match.min.js"></script>
+  <![endif]-->
 </head>
 <body>
   <div id="mount" class="full-container">${content}</div>
