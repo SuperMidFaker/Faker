79b4836ba3bdf5a79eb04f55e8ab4f7747bafadd
Carlos.fan <carlos_fan@126.com>
2016-01-28 11:38:09 +0800

    进口委托页面

--- client/containers/import/delegate.jsx
+++ client/containers/import/delegate.jsx
@@ -1,8 +1,9 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { submitDelegate, loadDelegates, updateId, delId, beginEdit, edit, cancelEdit } from '../../../universal/redux/reducers/importdelegate';
+import { submitDelegate, loadDelegates, updateId, delId, beginEdit, edit, cancelEdit,loadStatus } from '../../../universal/redux/reducers/importdelegate';
 import { isLoaded } from '../../../reusable/common/redux-actions';
 import connectFetch from '../../../reusable/decorators/connect-fetch';
+import SearchBar from '../../../reusable/components/search-bar';
 import cx from '../../../reusable/browser-util/classname-join';
 import { toNumber } from '../../../reusable/common/transformer'; 
 import {Table, Button, AntIcon, Form, Input, Radio, Row, Col, Datepicker, Select, message} from '../../../reusable/ant-ui';
@@ -12,21 +13,34 @@ const RadioGroup = Radio.Group;
 const InputGroup = Input.Group;
 
 function fetchData({ state, dispatch, cookie }) {
-  if (!isLoaded(state, 'importDelegate')) {
-    return dispatch(loadDelegates({ tenantId: state.account.tenantId }, cookie));
-  }
+    const promises = [];
+    !isLoaded(state, 'importdelegate')
+    {
+       let p =dispatch(loadDelegates(cookie, { 
+                tenantId: state.account.tenantId,
+                pageSize: state.importdelegate.idlist.pageSize,
+                currentPage: state.importdelegate.idlist.current
+            }));
+        promises.push(p);
+        
+        p = dispatch(loadStatus(cookie, {
+              tenantId: state.account.tenantId
+            }));
+        promises.push(p);
+    }
+  return Promise.all(promises);
 }
 
-@connectFetch({deferred: true})(fetchData)
+@connectFetch()(fetchData)
 @connect(
   state => ({
     tenantId: state.account.tenantId,
-    idlist: state.warehouse.idlist,
-    needUpdate: state.warehouse.needUpdate,
-    formData: state.warehouse.formData,
-    loading: state.warehouse.loading
+    idlist: state.importdelegate.idlist,
+    needUpdate: state.importdelegate.needUpdate,
+    formData: state.importdelegate.formData,
+    loading: state.importdelegate.loading
   }),
-  { updateId, delId, loadDelegates, submitDelegate, beginEdit, edit, cancelEdit }
+  { updateId, delId, loadDelegates, submitDelegate, beginEdit, edit, cancelEdit, loadStatus }
 )
 @Form.formify({
   mapPropsToFields: (props) => (props.formData),
@@ -50,6 +64,7 @@ export default class ImportDelegate extends React.Component {
     edit: PropTypes.func.isRequired,
     cancelEdit: PropTypes.func.isRequired,
     loadDelegates: PropTypes.func.isRequired,
+    loadStatus: PropTypes.func.isRequired,
     submitDelegate: PropTypes.func.isRequired,
     needUpdate: PropTypes.bool,
     tenantId: PropTypes.number.isRequired
@@ -57,7 +72,11 @@ export default class ImportDelegate extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showForm: false
+      showForm: false,
+      statusAll:'ghost',
+      statusNotSend:'primary',
+      statusNotAccept:'ghost',
+      statusAccept:'ghost',
     };
   }
   handleIdReg() {
@@ -102,14 +121,47 @@ export default class ImportDelegate extends React.Component {
       showForm: false
     });
   }
-  handleIdEdit(warehouse, index) {
-    this.props.beginEdit(warehouse, index);
+  handleIdEdit(importdelegate, index) {
+    this.props.beginEdit(importdelegate, index);
     this.setState({
       showForm: true
     });
   }
   handleIdRemove(idKey) {
     this.props.delId(idKey);
+  }
+  handleGetDateFromUTC(datestring) {
+      if(datestring.length>0) {
+          return datestring.substring(0,10);
+      }
+      
+  }
+  handleChangeStatus(status) {
+    this.setState({
+        statusAll:"statusAll"===status?"primary":"ghost",
+        statusNotSend:"statusNotSend"===status?"primary":"ghost",
+        statusNotAccept:"statusNotAccept"===status?"primary":"ghost",
+        statusAccept:"statusAccept"===status?"primary":"ghost"
+    });
+  }
+   handleSearch(searchVal) {
+    alert('123');
+    const filters = [[{
+      name: 'del_no',
+      value: searchVal
+    }, {
+      name: 'bill_no',
+      value: searchVal
+    }, {
+      name: 'invoice_no',
+      value: searchVal
+    }]];
+    this.props.loadDelegates(null, {
+      tenantId: this.props.tenant.id,
+      pageSize: this.props.idlist.pageSize,
+      currentPage: 1,
+      filters: JSON.stringify(filters)
+    });
   }
   renderValidateStyle(item) {
     const {isFieldValidating, getFieldError, getFieldsValue} = this.props.formhoc;
@@ -130,6 +182,7 @@ export default class ImportDelegate extends React.Component {
   }
   render() {
     const { idlist, loading, needUpdate, formhoc: {getFieldProps, getFieldError} } = this.props;
+    const { statusAll, statusAccept, statusNotAccept, statusNotSend } = this.state;
     const dataSource = new Table.DataSource({
       fetcher: (params) => this.props.loadDelegates(params),
       resolve: (result) => result.data,
@@ -176,10 +229,20 @@ export default class ImportDelegate extends React.Component {
       dataIndex: 'rec_tenant_id'
     }, {
       title: '委托时间',
-      dataIndex: 'del_date'
+      dataIndex: 'del_date',
+      render: (text, record, index) => {// todo down icon not horiztonal
+        return (<span>
+           {this.handleGetDateFromUTC(record.del_date)}
+            </span>);
+      }
     }, {
       title: '接单时间',
-      dataIndex: 'rec_del_date'
+      dataIndex: 'rec_del_date',
+        render: (text, record, index) => {// todo down icon not horiztonal
+        return (<span>
+           {this.handleGetDateFromUTC(record.rec_del_date)}
+            </span>);
+      }
     }, {
       title: '提单号',
       dataIndex: 'bill_no'
@@ -206,17 +269,25 @@ export default class ImportDelegate extends React.Component {
       }
     }];
     return (
-      <div className="table-wrapper">
-        <div className="row">
-          <div className={ this.state.showForm ? 'form-fade-leave' : '' }>
-            <div className="table-header">
-              <Button type="primary" size="large" onClick={ () => this.handleDgReg() }><AntIcon type="plus" /><span>新增</span></Button>
-            </div>
-            <div className="table-body">
-              <Table rowSelection={rowSelection} columns={ columns } loading={ loading } remoteData={ idlist } dataSource={ dataSource }/>
-            </div>
+        <div className="main-content">
+         <div className="page-header">
+          <div className="pull-right action-btns">
+            <SearchBar placeholder="业务单号/发票号/提单号" onInputSearch={(val) => this.handleSearch(val)} />
+            <a role="button">高级搜索</a>
           </div>
+          <h2>进口委托</h2>
+        </div>
+        <div className="page-body">
+          <div className="panel-header">
+              <Button type={statusAll} style={{marginRight:5}} onClick={ () => this.handleChangeStatus('statusAll') } ><span>全部</span></Button>
+              <Button type={statusNotSend} style={{marginRight:5}} onClick={ () => this.handleChangeStatus('statusNotSend') } ><span>未发送(2)</span></Button>
+              <Button type={statusNotAccept} style={{marginRight:5}} onClick={ () => this.handleChangeStatus('statusNotAccept') } ><span>未受理(0)</span></Button>
+              <Button type={statusAccept} style={{marginRight:5}} onClick={ () => this.handleChangeStatus('statusAccept') } ><span>已接单(0)</span></Button>
           </div>
+          <div className="panel-body body-responsive">
+             <Table rowSelection={rowSelection} columns={ columns } loading={ loading } remoteData={ idlist } dataSource={ dataSource }/>
+          </div>
+        </div>
       </div>
     );
   }
