7348528664057f986d9c2d702a02f0c57912e122
Carlos.fan <carlos_fan@126.com>
2016-01-28 11:36:09 +0800

    进口委托数据库操作文件

--- server/models/importDelegate.db.js
+++ server/models/importDelegate.db.js
@@ -1,14 +1,29 @@
 import mysql from '../../reusable/db-util/mysql';
+import sqlFormat from '../../reusable/db-util/sqlFormat';
 
 export default {
-  getIdTotalCount(tenantId) {
-    const sql = `select count(del_no) as count from g_bus_delegate`;
-    return mysql.query(sql);
+  getIdTotalCount(tenantId,filters ) {
+    const args = [];
+    const filterClause =sqlFormat.concatFilterSql(filters, args);
+    const sql = `select count(del_no) as count from g_bus_delegate where 1=1 ${filterClause}`;
+    console.log ( sql, args );
+    return mysql.query(sql, args);
   },
-  getPagedIdsByCorp(current, pageSize, tenantId) {
-    const sql = `select * from g_bus_delegate 
-      limit ?, ?`;
-    const args = [(current - 1) * pageSize, pageSize];
+  getPagedIdsByCorp(current, pageSize, filters, sortField, sortOrder, tenantId) {
+    const args = [];
+    const filterClause = sqlFormat.concatFilterSql(filters, args);
+    let sortColumn = sortField || 'del_id';
+    const sortClause = ` order by ${sortColumn} ${sortOrder === 'descend' ? 'desc' : 'asc'} `;
+    const sql = `select del_id as \`key\`,del_no,\`status\`,customs_status,del_date,invoice_no,bill_no,send_tenant_id,rec_tenant_id,creater_login_id,rec_login_id,rec_del_date,master_customs,declare_way_no,usebook,ems_no,trade_mode,urgent,delegate_type,tenant_id,other_note,created_date,update_date from g_bus_delegate where 1=1 ${filterClause} ${sortClause}  limit ?, ?`;
+    args.push((current - 1) * pageSize, pageSize);
+    console.log(sql, args);
     return mysql.query(sql,args);
+  },
+  getStatusCount(tenantId,status,filters) {
+    const args = [status];
+    const filterClause =sqlFormat.concatFilterSql(filters, args);
+    const sql = `select count(status) as count from g_bus_delegate where status=? ${filterClause}`;
+    console.log(sql, args);
+    return mysql.query(sql, args);
   }
 }
