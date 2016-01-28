df3fe59bb76c8e334a8cdf7269fb7ade002620a1
Carlos.fan <carlos_fan@126.com>
2016-01-28 11:35:36 +0800

    进口委托api

--- server/api/importDelegate.api.js
+++ server/api/importDelegate.api.js
@@ -3,27 +3,52 @@ import idDao from '../models/importDelegate.db';
 import Result from '../../reusable/node-util/response-result';
 
 export default [
-  ['get', '/v1/import/importdelegates', importdelegatesG] 
-//   ['post', '/v1/import/importdelegates', importdelegatesP],
+  ['get', '/v1/import/importdelegates', importdelegates],
+  ['get', '/v1/import/status', importdelegateStatusG]
 //   ['put', '/v1/import/importdelegates', updateDelegate],
 //   ['delete', '/v1/import/importdelegates', delDelegate]
 ]
 
-function *importdelegatesG() {
+function *importdelegates() {
   const current = parseInt(this.request.query.currentPage || 1, 10);
   const pageSize = parseInt(this.request.query.pageSize || 10, 10);
   const tenantId = parseInt(this.request.query.tenantId || 0, 10);
+
+  const filters = this.request.query.filters ? JSON.parse(this.request.query.filters) : [];
+  const sortField = this.request.query.sortField;
+  const sortOrder = this.request.query.sortOrder;
+  
   if (!tenantId) {
    // return Result.ParamError(this, 'corpid incorrect');
   }
   try {
-    const totals = yield idDao.getIdTotalCount(tenantId);
-    const ids = yield idDao.getPagedIdsByCorp(current, pageSize, tenantId);
+    const totals = yield idDao.getIdTotalCount(tenantId,filters);
+    const ids = yield idDao.getPagedIdsByCorp(current, pageSize, filters, sortField, sortOrder, tenantId);
     return Result.OK(this, {
       totalCount: totals.length > 0 ? totals[0].count : 0,
       pageSize,
       current,
       data: ids
+    });
+  } catch (e) {
+    console.log(e);
+    return Result.InternalServerError(this, e.message);
+  }
+}
+
+function *importdelegateStatusG() {
+  const tenantId = parseInt(this.request.query.tenantId || 0, 10);
+  const filters = this.request.query.filters ? JSON.parse(this.request.query.filters) : [];
+  
+  try {
+    const notSendCount = yield idDao.getStatusCount(tenantId,0,filters);
+    const notAcceptCount = yield idDao.getStatusCount(tenantId,1,filters);
+    const acceptCount = yield idDao.getStatusCount(tenantId,2,filters);
+    
+    return Result.OK(this, {
+      notSendCount,
+      notAcceptCount,
+      acceptCount
     });
   } catch (e) {
     console.log(e);
