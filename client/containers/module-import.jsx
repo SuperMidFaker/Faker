a7ba14f04a5fded4c3ad73f0313ee06cafabcb15
Eric Shao <eric.shao@qeemo.me>
2016-02-03 12:20:58 +0800

    修改“进口”菜单及图标

--- client/containers/module-import.jsx
+++ client/containers/module-import.jsx
@@ -8,33 +8,39 @@ export default class ImportM extends React.Component {
   render() {
     const linkMenus = [{
       single: true,
-      key: 'import-1',
+      key: 'import-0',
       path: '/import/',
-      icon: 's7-refresh-cloud',
+      icon: 's7-display1',
       text: '工作台'
     }, {
       single: true,
-      key: 'import-2',
+      key: 'import-1',
       path: '/import/delegate',
-      icon: 's7-refresh-cloud',
-      text: '业务委托'
+      icon: 's7-next-2',
+      text: '报关委托'
+    }, {
+      single: true,
+      key: 'import-2',
+      path: '/import/receive',
+      icon: 's7-mail-open-file',
+      text: '报关受理'
     }, {
       single: true,
       key: 'import-3',
       path: '/import/passage',
-      icon: 's7-refresh-cloud',
+      icon: 's7-note',
       text: '通关作业'
     }, {
       single: true,
       key: 'import-4',
       path: '/import/tracking',
-      icon: 's7-refresh-cloud',
-      text: '业务跟踪'
+      icon: 's7-look',
+      text: '通关跟踪'
     }, {
       single: true,
       key: 'import-5',
       path: '/import/billing',
-      icon: 's7-refresh-cloud',
+      icon: 's7-cash',
       text: '计费账单'
     }, {
       single: true,
