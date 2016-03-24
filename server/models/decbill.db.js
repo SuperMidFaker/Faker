ed1b6b88ae87ff4a37e70e7480da674e7615b8a8
Kurten Chan <kurten.chan@yun56.co>
2016-03-24 11:55:02 +0800

    models:添加sql字段

--- server/models/decbill.db.js
+++ server/models/decbill.db.js
@@ -1,5 +1,64 @@
 import db from '../db-util/mysql';
 
+const cols = ['del_no', 'delegate_type', 'category', 'declare_way_no', 'seq_no', 'input_date', 'customer_name', 's_bus_no', 'invoice_no', 'master_customs', 'entry_id', 'i_e_port', 'ems_no', 'i_e_date', 'd_date', 'trade_co', 'trade_name', 'traf_mode', 'traf_name', 'bill_no', 'owner_code', 'owner_name', 'trade_mode', 'cut_mode', 'pay_mode', 'license_no', 'trade_country', 'distinate_port', 'district_code', 'appr_no', 'trans_mode', 'fee_curr', 'fee_rate', 'fee_mark', 'insur_curr', 'insur_rate', 'insur_mark', 'other_curr', 'other_rate', 'other_mark', 'contr_no', 'pack_no', 'wrap_type', 'gross_wt', 'net_wt', 'jzxsl', 'cert_mark', 'use_to', 'note', 'voyage_no', 'agent_code', 'agent_name', 'library_no', 'prdtid', 'storeno', 'ramanualno', 'radeclno', 'trade_term', 'forwarder', 'cont_40', 'cont_20', 'cont_lcl', 'partner_id', 'jzx_type', 'order_date', 'mawb_no', 'hawb_no'];
+
+/*
+del_id
+external_no
+creater_login_id
+customer_id
+templat_no
+year
+month
+week
+dec_mode ?
+tenant_id
+create_tenant_id
+ */
+
+// const colsList =
+/*
+
+del_no
+ems_no
+list_g_no
+em_g_no
+cop_g_no
+code_t
+code_s
+g_name
+g_model
+country_code
+element
+qty
+unit
+factor_1
+unit_1
+factor_2
+unit_2
+dec_total
+dec_price
+curr
+gross_wt
+wet_wt
+use_type
+duty_mode
+control_ma
+bom_version
+invoice_no
+order_no
+note
+ent_seq_no
+ent_g_no
+org_seq_no
+org_g_no
+
+id
+del_id
+tenant_id
+create_tenant_id
+ */
+
 export default {
   insertHead(head) {
 
