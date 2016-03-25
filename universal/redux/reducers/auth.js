d0e314a6b8d5129fe1e7e03dd9c2e51016ef4747
kayw <kayw@outlook.com>
2016-03-25 19:28:39 +0800

    fix: 忘记密码页面去掉enter/leave的reducer action

--- universal/redux/reducers/auth.js
+++ universal/redux/reducers/auth.js
@@ -5,8 +5,6 @@ const LOGIN = '@@qm-auth/auth/LOGIN';
 const LOGIN_SUCCEED = '@@qm-auth/auth/LOGIN_SUCCEED';
 const LOGIN_FAIL = '@@qm-auth/auth/LOGIN_FAIL';
 const INPUT_CHANGE = '@@qm-auth/auth/INPUT_CHANGE';
-const LEAVE_FORGOT_PAGE = '@@qm-auth/auth/LEAVE_FORGOT_PAGE';
-const ENTER_FORGOT_PAGE = '@@qm-auth/auth/ENTER_FORGOT_PAGE';
 const SMS_REQUEST = '@@qm-auth/auth/SMS_REQUEST';
 const SMS_REQUEST_SUCCEED = '@@qm-auth/auth/SMS_REQUEST_SUCCEED';
 const SMS_REQUEST_FAIL = '@@qm-auth/auth/SMS_REQUEST_FAIL';
@@ -19,8 +17,8 @@ const initialState = {
   remember: false,
   loggingIn: false,
   isAuthed: false,
-  verified: false,
   nonTenant: false,
+  userId: null,
   smsId: null
 };
 
@@ -68,16 +66,10 @@ export default function reducer(state = initialState, action = {}) {
           message: action.error.msg
         }
       };
-    case ENTER_FORGOT_PAGE:
-      return { ...state, error: null, smsId: null, verified: false };
     case SMS_REQUEST_SUCCEED:
-      return { ...state, error: null, smsId: action.result.data.smsId, userId: action.result.data.userId };
-    case SMS_REQUEST_FAIL:
-      return { ...state, error: { message: action.error.msg } };
+      return { ...state, smsId: action.result.data.smsId, userId: action.result.data.userId };
     case SMS_VERIFY_SUCCEED:
-      return { ...state, error: null, verified: true };
-    case SMS_VERIFY_FAIL:
-      return { ...state, error: { message: action.error.msg } };
+      return { ...state, smsId: null, userId: null };
     default:
       return state;
   }
@@ -104,7 +96,7 @@ export function setValue(field, value) {
 export function requestSms(phone) {
   return {
     [CLIENT_API]: {
-      types: [SMS_REQUEST, SMS_REQUEST_SUCCEED, SMS_REQUEST_FAIL],
+      types: [ SMS_REQUEST, SMS_REQUEST_SUCCEED, SMS_REQUEST_FAIL ],
       endpoint: 'public/v1/sms/code',
       method: 'post',
       data: { phone }
@@ -115,22 +107,10 @@ export function requestSms(phone) {
 export function verifySms(smsId, userId, smsCode, newPwd) {
   return {
     [CLIENT_API]: {
-      types: [SMS_VERIFY, SMS_VERIFY_SUCCEED, SMS_VERIFY_FAIL],
+      types: [ SMS_VERIFY, SMS_VERIFY_SUCCEED, SMS_VERIFY_FAIL ],
       endpoint: 'public/v1/sms/verify',
       method: 'post',
       data: { smsId, userId, smsCode, newPwd }
     }
-  };
-}
-
-export function leaveForgot() {
-  return {
-    type: LEAVE_FORGOT_PAGE
-  };
-}
-
-export function enterForgot() {
-  return {
-    type: ENTER_FORGOT_PAGE
   };
 }
