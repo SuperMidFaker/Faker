1af5c719a9cc485be09efdcbd6c990ed74419181
Kurten Chan <kurten.chan@yun56.co>
2016-03-29 19:24:45 +0800

    server:实现打包sql结构

--- server/openapi/apis/customs.api.js
+++ server/openapi/apis/customs.api.js
@@ -18,6 +18,90 @@ import decbillDao from '../../models/decbill.db';
 import entryDao from '../../models/entry.db';
 import codes from '../codes';
 
+function billHeadToEntryHead(entryId, head) {
+  return {
+    entry_id: entryId,
+    del_no: head.del_no,
+    i_e_flag: head.delegate_type === 0 ? 'I' : 'E',
+    i_e_port: head.i_e_port,
+    i_e_date: head.i_e_date,
+    d_date: head.d_date,
+    destination_port: head.distinate_port,
+    traf_name: head.traf_name,
+    traf_mode: head.traf_mode,
+    trade_co: head.trade_co,
+    trade_name: head.trade_name,
+    district_code: head.district_code,
+    owner_code: head.owner_code,
+    owner_name: head.owner_name,
+    agent_code: head.agent_code,
+    agent_name: head.agent_name,
+    voyage_no: head.voyage_no,
+    contr_no: head.contr_no,
+    bill_no: head.bill_no,
+    trade_country: head.trade_country,
+    trade_mode: head.trade_mode,
+    cut_mode: head.cut_mode,
+    trans_mode: head.trans_mode,
+    container_no: head.jzxsl,
+    pay_way: head.pay_mode,
+    fee_mark: head.fee_mark,
+    fee_curr: head.fee_curr,
+    fee_rate: head.fee_rate,
+    insur_mark: head.insur_mark,
+    insur_curr: head.insur_curr,
+    insur_rate: head.insur_rate,
+    other_mark: head.other_mark,
+    other_curr: head.other_curr,
+    other_rate: head.other_rate,
+    pack_no: head.pack_no,
+    gross_wt: head.gross_wt,
+    net_wt: head.net_wt,
+    wrap_type: head.wrap_type,
+    manual_no: head.ems_no,
+    license_no: head.license_no,
+    appr_no: head.appr_no,
+    node_s: head.note,
+    decl_port: head.master_customs,
+    tenant_id: head.tenant_id
+  };
+}
+
+function billHeadsToEntryHeads(ids, head) {
+  const eIds = ids.split(',');
+  if (eIds.length === 0) {
+    return [];
+  }
+  const eArr = [];
+  eIds.forEach(id => {
+    eArr.push(billHeadToEntryHead(id, head));
+  });
+
+  return eArr;
+}
+
+function billHeadToDelegate(head) {
+  return {
+    del_no: head.del_no,
+    status: 1,
+    del_date: head.input_date,
+    invoice_no: head.invoice_no,
+    bill_no: head.bill_no,
+    send_tenant_id: head.tenant_id,
+    rec_tenant_id: head.create_tenant_id,
+    rec_del_date: head.input_date,
+    master_customs: head.master_customs,
+    declare_way_no: head.declare_way_no,
+    usebook: head.ems_no ? 1 : 0,
+    ems_no: head.ems_no,
+    trade_mode: head.trade_mode,
+    urgent: 0,
+    delegate_type: head.delegate_type,
+    tenant_id: head.tenant_id,
+    created_date: new Date()
+  };
+}
+
 function *billImport() {
   const bills = this.reqbody.bills;
 
@@ -42,18 +126,7 @@ function *billImport() {
           bill.lists[j].tenant_id = tenant.tenant_id;
           bill.lists[j].create_tenant_id = ctenant.tenant_id;
         }
-        const eIds = entryId.split(',');
-        const eArr = [];
-        eIds.forEach(id => {
-          eArr.push({
-            entry_id: id,
-            del_no: bill.head.del_no,
-            i_e_flag: bill.head.delegate_type === 0 ? 'I' : 'E',
-            i_e_port: bill.head.master_customs,
-            destination_port: bill.head.distinate_port,
-            // TODO
-          });
-        });
+
         yield [decbillDao.insertHead(bill.head),
           decbillDao.insertLists(bill.lists)];
       }
