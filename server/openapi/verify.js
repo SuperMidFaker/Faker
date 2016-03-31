1080213b03aa356bd1ca81b619e48fd4f21ce2f7
Kurten Chan <kurten.chan@yun56.co>
2016-03-31 14:38:16 +0800

    openapi:新增根据报关单号获取报关单历史log接口

--- server/openapi/verify.js
+++ server/openapi/verify.js
@@ -29,9 +29,10 @@ export default function *apiAuth(next) {
         return this.error(codes.missing_content_type);
       }
       this.reqbody = yield parse(this.req);
-      if (!this.reqbody.access_token) {
-        return this.forbidden(codes.access_token_not_valid);
-      }
+    }
+
+    if (!this.reqbody.access_token) {
+      return this.forbidden(codes.access_token_not_valid);
     }
 
     const res = yield appDao.getAuthByAccessToken(this.reqbody.access_token);
