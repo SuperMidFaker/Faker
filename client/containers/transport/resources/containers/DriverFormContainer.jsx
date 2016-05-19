ff10839ef9d78e7827747a1d6995e037bde2ff6f
叶伟伟 <yww7289@163.com>
2016-05-19 16:15:58 +0800

    fix: 添加和编辑返回的时候,对应的位置高亮

--- client/containers/transport/resources/containers/DriverFormContainer.jsx
+++ client/containers/transport/resources/containers/DriverFormContainer.jsx
@@ -19,11 +19,11 @@ import { setNavTitle } from 'universal/redux/reducers/navbar';
 @Form.formify()
 export default class DriverFormContainer extends Component {
   static propTypes = {
-    form: PropTypes.object.isRequired,    // @Form.formify创建的form对象
-    params: PropTypes.object.isRequired,  // 用于获取:driver_id参数
-    drivers: PropTypes.array.isRequired,  // 服务器返回的司机数组
-    dispatch: PropTypes.func.isRequired,  // store.dispatch函数
-    addDriver: PropTypes.func.isRequied,  // 增加车辆的actionCreator
+    form: PropTypes.object.isRequired,     // @Form.formify创建的form对象
+    params: PropTypes.object.isRequired,   // 用于获取:driver_id参数
+    drivers: PropTypes.array.isRequired,   // 服务器返回的司机数组
+    dispatch: PropTypes.func.isRequired,   // store.dispatch函数
+    addDriver: PropTypes.func.isRequired,  // 增加车辆的actionCreator
     editDriver: PropTypes.func.isRequired, // 修改车辆信息的actionCreator
     tenantId: PropTypes.number.isRequired,
   }
