a51d9f77cc569d02348e651de31bc0ec22f4c8c0
kayw <kayw@outlook.com>
2016-05-27 15:48:00 +0800

    update ant design 1.1.0

--- common/reducers/form-common.js
+++ common/reducers/form-common.js
@@ -3,8 +3,7 @@ import { CLIENT_API } from 'common/reduxMiddlewares/requester';
 export function appendFormAcitonTypes(domain, actypes) {
   ['FORM_LOAD', 'FORM_LOAD_SUCCEED', 'FORM_LOAD_FAIL',
   'FORM_ASSIGN', 'FORM_CLEAR', 'SET_FORM_VALUE'].forEach(
-    /* eslint-disable no-param-reassign */
-    act => actypes[act] = `${domain}${act}`);
+    act => actypes[act] = `${domain}${act}`); /* eslint-disable no-param-reassign */
 }
 /* eslint-enable no-param-reassign */
 
