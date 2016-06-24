b4aee90b28cacd2dbbb3a0fa118a5ac9d5644ba1
Eric Shao <eric.shao@welogix.cn>
2016-06-24 12:09:27 +0800

    Changed “清除选择” to close button

--- client/apps/cms/common/declare/list.jsx
+++ client/apps/cms/common/declare/list.jsx
@@ -158,7 +158,7 @@ export default class DeclareList extends React.Component {
             />
           </div>
           <div className={`bottom-fixed-row ${this.state.selectedRowKeys.length === 0 ? 'hide' : ''}`}>
-            <Button size="large" onClick={this.handleSelectionClear} className="pull-right">
+            <Button shape="circle-outline" icon="cross-circle-o" onClick={this.handleSelectionClear} className="pull-right">
             {formatContainerMsg(intl, 'clearSelection')}
             </Button>
           </div>
