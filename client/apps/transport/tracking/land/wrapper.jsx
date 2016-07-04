46055ce90c785d5dfb1f1845a570f3403ffd2b77
Eric Shao <eric.shao@welogix.cn>
2016-07-04 23:44:36 +0800

    added import Button

--- client/apps/transport/tracking/land/wrapper.jsx
+++ client/apps/transport/tracking/land/wrapper.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { intlShape, injectIntl } from 'react-intl';
-import { Radio } from 'ant-ui';
+import { Radio, Button } from 'ant-ui';
 import { format } from 'client/common/i18n/helpers';
 import NavLink from 'client/components/nav-link';
 import messages from './message.i18n';
