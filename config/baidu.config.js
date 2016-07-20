bae9f127135acbf75a061e978b3e077e0baed573
kayw <kayw@outlook.com>
2016-07-20 16:53:01 +0800

    remove api related

--- config/baidu.config.js
+++
@@ -1,7 +0,0 @@
-const config = new Map();
-const env = process.env.NODE_ENV = (process.env.NODE_ENV || 'development').trim();
-
-config.set('AK_WELOGIX_WEB', 'h9mZgQCfWCsEf9axutFUlgPhgbVGmng5');
-config.set('AK_WELOGIX_SERVER', 'KSQ06zaqrY8d05PKkaolmFhSMYVLw4YX');
-
-export default config;
