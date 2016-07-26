23feaef4a812b2f7b3b5d179b53524ee3443f76d
kayw <kayw@outlook.com>
2016-07-26 23:33:55 +0800

    readd weixin auth middleware

---
+++ server/middlewares/weixin-auth.js
@@ -0,0 +1,36 @@
+import superagent from 'superagent';
+
+function isWxAccessUrl(url) {
+  return url.indexOf('/weixin') === 0;
+}
+// 微信公众平台页面访问对openid与loginid的中间处理
+export default () =>
+  function* weixinMPAuthMw(next) {
+    const ua = this.request.get('user-agent');
+    const isWeixin = /MicroMessenger/i.test(ua)
+    if (!isWeixin && !isWxAccessUrl(this.request.url)) {
+      yield next;
+      return;
+    }
+    const authRes = yield superagent.get(`${API_ROOTS.default}public/v1/weixin/auth`)
+      .set('cookies', this.cookies)
+      .query({ code: this.request.query.code });
+    if (authRes.unauthed) {
+      // 认为是第一次访问,跳转至公众号授权地址, 返回为请求地址
+      this.redirect(weixinOAuth.genCodeUrl(this.request.path));
+    } else if (authRes.exceptional) {
+      this.throw(403, {
+        msg: authRes.message,
+      });
+    } else {
+      this.cookies.set(authRes.cookies);
+      if (authRes.unbind) {
+        // 用户不存在,需登录绑定
+        this.redirect(`/weixin/bind?next=${encodeURIComponent(this.request.path)}`);
+      } else if (authRes.resend) {
+        this.redirect(this.request.path); // 使用新cookie重新请求页面
+      } else {
+        yield next;
+      }
+    }
+  };
