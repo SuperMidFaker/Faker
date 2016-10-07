62c300b0613c39368fad639e1716fef5598ce464
Eric Shao <eric.shao@welogix.cn>
2016-10-07 22:55:08 +0800

    fixed minor issues

--- client/components/notification-popover.jsx
+++ client/components/notification-popover.jsx
@@ -252,7 +252,7 @@ export default class NotificationPopover extends React.Component {
       />
       </div>
       <div className="popover-footer">
-        <NavLink to="/my/messages">See all notifications</NavLink>
+        <NavLink to="/my/messages">{this.msg('seeAll')}</NavLink>
       </div>
     </div>);
   }
