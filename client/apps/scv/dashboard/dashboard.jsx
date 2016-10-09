ff46fc9012974188e26cfba16c0a14e64cd6a277
Eric Shao <eric.shao@welogix.cn>
2016-10-09 14:27:44 +0800

    added mockup dashboard for SCV

--- client/apps/scv/dashboard/dashboard.jsx
+++ client/apps/scv/dashboard/dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { intlShape, injectIntl } from 'react-intl';
-import { Card, Row, Col } from 'antd';
+import { Alert, Card, Row, Col } from 'antd';
 import QueueAnim from 'rc-queue-anim';
 import connectNav from 'client/common/decorators/connect-nav';
 import { format } from 'client/common/i18n/helpers';
@@ -24,13 +24,50 @@ export default class SCVDashboard extends React.Component {
     intl: intlShape.isRequired,
     tenantId: PropTypes.number.isRequired,
   }
-  msg = key => formatMsg(this.props.intl, key);
 
+  componentDidMount() {
+    const data = [
+        { name: 'Jan', Import: 12814, Export: 3054 },
+        { name: 'Feb', Import: 13012, Export: 5067 },
+        { name: 'Mar', Import: 11624, Export: 4004 },
+        { name: 'Apr', Import: 8814, Export: 7054 },
+        { name: 'May', Import: 12998, Export: 2043 },
+        { name: 'Jun', Import: 12321, Export: 6507 },
+        { name: 'Jul', Import: 10342, Export: 3019 },
+        { name: 'Aug', Import: 22998, Export: 5243 },
+        { name: 'Sep', Import: 11261, Export: 4419 },
+        { name: 'Oct', Import: 2651, Export: 199 },
+        { name: 'Nov', Import: 0, Export: 0 },
+        { name: 'Dec', Import: 0, Export: 0 },
+    ];
+    const Frame = G2.Frame;
+    let frame = new Frame(data);
+    frame = Frame.combinColumns(frame, ['Import', 'Export'], 'Revenue', 'I/E', 'name');
+    const chart = new G2.Chart({
+      id: 'c1',
+      width: 1000,
+      height: 300,
+    });
+    chart.source(frame, {
+      Revenue: {
+        alias: '进出口总额（美元）',
+        formatter(val) {
+          return `${parseInt(val / 1000, 10)}k`;
+        },
+      },
+      name: {
+        alias: '2016年',
+      },
+    });
+    chart.areaStack().position('name*Revenue').color('I/E');
+    chart.render();
+  }
   handleRadioChange = (ev) => {
     if (ev.target.value === this.props.listFilter.status) {
       return;
     }
   }
+  msg = key => formatMsg(this.props.intl, key);
   render() {
     return (
       <QueueAnim type={['bottom', 'up']}>
@@ -39,33 +76,122 @@ export default class SCVDashboard extends React.Component {
           </div>
           <span>{this.msg('dashboardTitle')}</span>
         </header>
-        <div className="main-content" key="main">
+        <div className="main-content">
           <div className="page-body card-wrapper">
             <Row gutter={16}>
               <Col span={16}>
-                <Card title="Order">
-                  hello
+                <Card title={this.msg('orders')} style={{ display: 'none' }}>
+                  <ul className="statistics-columns">
+                    <li className="col-4">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('accepted')}</h6>
+                        <p className="data-num">12</p>
+                      </div>
+                    </li>
+                    <li className="col-4">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('sent')}</h6>
+                        <p className="data-num">6</p>
+                      </div>
+                    </li>
+                    <li className="col-4">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('pickedup')}</h6>
+                        <p className="data-num">23</p>
+                      </div>
+                    </li>
+                    <li className="col-4">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('delivered')}</h6>
+                        <p className="data-num"></p>
+                      </div>
+                    </li>
+                    <li className="col-4">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('completed')}</h6>
+                        <p className="data-num">32</p>
+                      </div>
+                    </li>
+                  </ul>
                 </Card>
-                <Card title="Shipment">
-                  hello
+                <Card title={this.msg('shipments')}>
+                  <ul className="statistics-columns">
+                    <li className="col-4">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('atOrigin')}</h6>
+                        <p className="data-num">12</p>
+                      </div>
+                    </li>
+                    <li className="col-4">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('inTransit')}</h6>
+                        <p className="data-num">6</p>
+                      </div>
+                    </li>
+                    <li className="col-4">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('arrived')}</h6>
+                        <p className="data-num">23</p>
+                      </div>
+                    </li>
+                    <li className="col-4">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('clearance')}</h6>
+                        <p className="data-num">23</p>
+                      </div>
+                    </li>
+                    <li className="col-4">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('inland')}</h6>
+                        <p className="data-num">32</p>
+                      </div>
+                    </li>
+                    <li className="col-4">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('delivered')}</h6>
+                        <p className="data-num">32</p>
+                      </div>
+                    </li>
+                  </ul>
                 </Card>
-                <Card title="Payment">
-                  hello
+                <Card title={this.msg('payments')}>
+                  <ul className="statistics-columns">
+                    <li className="col-8">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('tax')}</h6>
+                        <p className="data-num">￥12,578.95</p>
+                      </div>
+                    </li>
+                    <li className="col-8">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('freightBills')}</h6>
+                        <p className="data-num">￥67,231.53</p>
+                      </div>
+                    </li>
+                    <li className="col-8">
+                      <div className="statistics-cell">
+                        <h6>{this.msg('brokerBills')}</h6>
+                        <p className="data-num">￥32,345.06</p>
+                      </div>
+                    </li>
+                  </ul>
                 </Card>
               </Col>
               <Col span={8}>
-                <Card title="Alerts" style={{ height: '100%' }}>
-                  hello
+                <Card title={this.msg('alerts')} >
+                  <Alert message="报关单221020161109715501 海关查验" type="warning" showIcon />
+                  <Alert message="1067172 送货延迟：收货人外地出差，18号以后送货" type="warning" showIcon />
                 </Card>
               </Col>
               <Col span={24}>
-                <Card title="Statistics">
-                    hello
+                <Card title={this.msg('statistics')}>
+                  <div id="c1"></div>
                 </Card>
               </Col>
             </Row>
           </div>
         </div>
+
       </QueueAnim>
     );
   }
--- client/apps/scv/dashboard/message.i18n.js
+++ client/apps/scv/dashboard/message.i18n.js
@@ -5,6 +5,62 @@ const messages = defineMessages({
     id: 'scv.dashboard.title',
     defaultMessage: '看板',
   },
+  orders: {
+    id: 'scv.dashboard.orders',
+    defaultMessage: '订单',
+  },
+  shipments: {
+    id: 'scv.dashboard.shipments',
+    defaultMessage: '货运',
+  },
+  payments: {
+    id: 'scv.dashboard.payments',
+    defaultMessage: '结算',
+  },
+  alerts: {
+    id: 'scv.dashboard.alerts',
+    defaultMessage: '预警',
+  },
+  statistics: {
+    id: 'scv.dashboard.statistics',
+    defaultMessage: '统计',
+  },
+  atOrigin: {
+    id: 'scv.dashboard.shipments.at.origin',
+    defaultMessage: '启运地',
+  },
+  inTransit: {
+    id: 'scv.dashboard.shipments.in.transit',
+    defaultMessage: '运输中',
+  },
+  arrived: {
+    id: 'scv.dashboard.shipments.arrived',
+    defaultMessage: '到港',
+  },
+  clearance: {
+    id: 'scv.dashboard.shipments.clearance',
+    defaultMessage: '清关',
+  },
+  inland: {
+    id: 'scv.dashboard.shipments.inland',
+    defaultMessage: '转运',
+  },
+  delivered: {
+    id: 'scv.dashboard.shipments.delivered',
+    defaultMessage: '收货',
+  },
+  tax: {
+    id: 'scv.dashboard.payments.tax',
+    defaultMessage: '缴税',
+  },
+  freightBills: {
+    id: 'scv.dashboard.payments.bills.freight',
+    defaultMessage: '货代账单',
+  },
+  brokerBills: {
+    id: 'scv.dashboard.payments.bills.brokder',
+    defaultMessage: '报关行账单',
+  },
 });
 
 export default messages;
