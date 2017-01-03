644e299d082122edf3faacd5d9efa6cd43585ea2
kayw <kayw@outlook.com>
2017-01-03 17:06:04 +0800

    duplicate messageid

--- client/admin/sso/message.i18n.js
+++ client/admin/sso/message.i18n.js
@@ -2,95 +2,95 @@ import { defineMessages } from 'react-intl';
 
 const messages = defineMessages({
   slogan: {
-    id: 'container.slogan',
+    id: 'admin.container.slogan',
     defaultMessage: '微骆云台运营管理系统',
   },
   loginEmptyParam: {
-    id: 'sso.login.empty.param',
+    id: 'admin.login.empty.param',
     defaultMessage: '用户名或密码为空',
   },
   pwdErrorParam: {
-    id: 'sso.login.error.param',
+    id: 'admin.login.error.param',
     defaultMessage: '密码有误',
   },
   loginUserNotFound: {
-    id: 'sso.login.user.notfound',
+    id: 'admin.login.user.notfound',
     defaultMessage: '用户{username}不存在',
   },
   loginExceptionError: {
-    id: 'sso.login.error.exception',
+    id: 'admin.login.error.exception',
     defaultMessage: '登录异常',
   },
   invalidPhone: {
-    id: 'sso.forgot.invalid.phone',
+    id: 'admin.forgot.invalid.phone',
     defaultMessage: '手机号码错误',
   },
   phoneNotfound: {
-    id: 'sso.forgot.notfound.phone',
+    id: 'admin.forgot.notfound.phone',
     defaultMessage: '手机号不存在,请先添加',
   },
   requestCodeException: {
-    id: 'sso.forgot.requestcode.exception',
+    id: 'admin.forgot.requestcode.exception',
     defaultMessage: '请求验证码异常',
   },
   invalidSmsCode: {
-    id: 'sso.forgot.invalid.smscode',
+    id: 'admin.forgot.invalid.smscode',
     defaultMessage: '验证码错误',
   },
   smsCodeVerifyException: {
-    id: 'sso.forgot.smscode.verify.exception',
+    id: 'admin.forgot.smscode.verify.exception',
     defaultMessage: '验证短信码异常',
   },
   subdomainNotFound: {
-    id: 'sso.login.subdomain.notfound',
+    id: 'admin.login.subdomain.notfound',
     defaultMessage: '当前子域未对应任何租户',
   },
   userPlaceholder: {
-    id: 'sso.user.placeholder',
+    id: 'admin.user.placeholder',
     defaultMessage: '用户名/手机号',
   },
   pwdPlaceholder: {
-    id: 'sso.pwd.placeholder',
+    id: 'admin.pwd.placeholder',
     defaultMessage: '密码',
   },
   login: {
-    id: 'sso.login',
+    id: 'admin.login',
     defaultMessage: '登录',
   },
   forgotPwd: {
-    id: 'sso.forgot.password',
+    id: 'admin.forgot.password',
     defaultMessage: '忘记密码',
   },
   remembered: {
-    id: 'sso.login.remembered',
+    id: 'admin.login.remembered',
     defaultMessage: '记住我',
   },
   verifyCodeGuide: {
-    id: 'sso.forgot.guide',
+    id: 'admin.forgot.guide',
     defaultMessage: '点击获取验证码,我们将向该号码发送免费的短信验证码以重置密码.',
   },
   phonePlaceholder: {
-    id: 'sso.forgot.phone.placeholder',
+    id: 'admin.forgot.phone.placeholder',
     defaultMessage: '登录手机号',
   },
   verifyObtatin: {
-    id: 'sso.forgot.verify.obtain',
+    id: 'admin.forgot.verify.obtain',
     defaultMessage: '获取验证码',
   },
   smsCodeSent: {
-    id: 'sso.forgot.smscode.sent',
+    id: 'admin.forgot.smscode.sent',
     defaultMessage: '验证码已经发送到您的手机:',
   },
   smsCode: {
-    id: 'sso.forgot.smscode',
+    id: 'admin.forgot.smscode',
     defaultMessage: '短信验证码',
   },
   newPwdPlaceholder: {
-    id: 'sso.forgot.newPwd.placeholder',
+    id: 'admin.forgot.newPwd.placeholder',
     defaultMessage: '新密码',
   },
   finishVerify: {
-    id: 'sso.forgot.finishVerify',
+    id: 'admin.forgot.finishVerify',
     defaultMessage: '完成验证',
   },
 });
