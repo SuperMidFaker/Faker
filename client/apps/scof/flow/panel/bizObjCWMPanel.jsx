2990a3456729cc3dbcf7565bd6b2478f8cc8dff4
Eric Shao <eric.shao@welogix.cn>
2017-03-10 17:27:11 +0800

    UI improvements Added  TMS/CWM panels initially

---
+++ client/apps/scof/flow/panel/bizObjCWMPanel.jsx
@@ -0,0 +1,52 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Row, Col, Card, Tabs } from 'antd';
+import { intlShape, injectIntl } from 'react-intl';
+import { loadCmsBizParams } from 'common/reducers/scofFlow';
+import FlowNodePanel from './flowNodePanel';
+import DelegationPane from './pane/cmsDelegationPane';
+import DeclManifestPane from './pane/cmsDeclManifestPane';
+import { formatMsg } from '../message.i18n';
+
+const TabPane = Tabs.TabPane;
+
+@injectIntl
+@connect(
+  state => ({
+    tenantId: state.account.tenantId,
+  }),
+  { loadCmsBizParams }
+)
+export default class FlowCwmNodePanel extends Component {
+  static propTypes = {
+    intl: intlShape.isRequired,
+    form: PropTypes.object.isRequired,
+  }
+  componentDidMount() {
+    this.props.loadCmsBizParams(this.props.tenantId);
+  }
+  msg = formatMsg(this.props.intl)
+  render() {
+    const { form } = this.props;
+    return (
+      <Row gutter={16}>
+        <Col sm={24} md={8}>
+          <Card title={this.msg('cwmFlowNode')} bodyStyle={{ padding: 0 }}>
+            <FlowNodePanel form={form} />
+          </Card>
+        </Col>
+        <Col sm={24} md={16}>
+          <Card title={this.msg('bizObject')} bodyStyle={{ padding: 0 }}>
+            <Tabs defaultActiveKey="objReceiving">
+              <TabPane tab={this.msg('objReceiving')} key="objReceiving">
+                <DelegationPane form={form} />
+              </TabPane>
+              <TabPane tab={this.msg('objShipping')} key="objShipping" >
+                <DeclManifestPane form={form} />
+              </TabPane>
+            </Tabs>
+          </Card>
+        </Col>
+      </Row>);
+  }
+}
