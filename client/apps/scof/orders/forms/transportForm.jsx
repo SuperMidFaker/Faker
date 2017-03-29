2e97ab438396eba182f1d2b72b92a57ee034d370
kayw <kayw@outlook.com>
2017-03-29 16:37:06 +0800

    scof order consigner consignee data

--- client/apps/scof/orders/forms/transportForm.jsx
+++ client/apps/scof/orders/forms/transportForm.jsx
@@ -46,6 +46,7 @@ export default class TransportForm extends Component {
           const consignee = formRequires.consigneeLocations.filter(cl => cl.node_id === nodedata.consignee_id)[0];
           const transitMode = formRequires.transitModes.filter(tm => tm.mode_code === nodedata.transit_mode)[0];
           this.handleSetClientForm({
+            consigner_id: nodedata.consigner_id,
             consigner_name: consigner && consigner.name,
             consigner_province: consigner && consigner.province,
             consigner_city: consigner && consigner.city,
@@ -56,6 +57,7 @@ export default class TransportForm extends Component {
             consigner_email: consigner && consigner.email,
             consigner_contact: consigner && consigner.contact,
             consigner_mobile: consigner && consigner.mobile,
+            consignee_id: nodedata.consignee_id,
             consignee_name: consignee && consignee.name,
             consignee_province: consignee && consignee.province,
             consignee_city: consignee && consignee.city,
@@ -89,31 +91,101 @@ export default class TransportForm extends Component {
     this.handleSetClientForm({ [key]: value });
   }
   handleConsignChange = (key, value) => {
+    if (typeof value !== 'string') {
+      return;
+    }
+    const consignForm = {};
+    if (key === 'consigner_name') {
+      const consign = this.props.formRequires.consignerLocations.find(item => item.name === value);
+      if (consign) {
+        consignForm.consigner_name = consign.name;
+        consignForm.consigner_province = consign.province;
+        consignForm.consigner_city = consign.city;
+        consignForm.consigner_district = consign.district;
+        consignForm.consigner_street = consign.street;
+        consignForm.consigner_region_code = consign.region_code;
+        consignForm.consigner_addr = consign.addr;
+        consignForm.consigner_email = consign.email;
+        consignForm.consigner_contact = consign.contact;
+        consignForm.consigner_mobile = consign.mobile;
+      } else if (!value) {
+        consignForm.consigner_id = null;
+        consignForm.consigner_name = null;
+        consignForm.consigner_province = null;
+        consignForm.consigner_city = null;
+        consignForm.consigner_district = null;
+        consignForm.consigner_street = null;
+        consignForm.consigner_region_code = null;
+        consignForm.consigner_addr = null;
+        consignForm.consigner_email = null;
+        consignForm.consigner_contact = null;
+        consignForm.consigner_mobile = null;
+      } else {
+        consignForm.consigner_id = null;
+      }
+    } else if (key === 'consignee_name') {
+      const consign = this.props.formRequires.consigneeLocations.find(item => item.name === value);
+      if (consign) {
+        consignForm.consignee_name = consign.name;
+        consignForm.consignee_province = consign.province;
+        consignForm.consignee_city = consign.city;
+        consignForm.consignee_district = consign.district;
+        consignForm.consignee_street = consign.street;
+        consignForm.consignee_region_code = consign.region_code;
+        consignForm.consignee_addr = consign.addr;
+        consignForm.consignee_email = consign.email;
+        consignForm.consignee_contact = consign.contact;
+        consignForm.consignee_mobile = consign.mobile;
+      } else if (!value) {
+        consignForm.consignee_id = null;
+        consignForm.consignee_name = null;
+        consignForm.consignee_province = null;
+        consignForm.consignee_city = null;
+        consignForm.consignee_district = null;
+        consignForm.consignee_street = null;
+        consignForm.consignee_region_code = null;
+        consignForm.consignee_addr = null;
+        consignForm.consignee_email = null;
+        consignForm.consignee_contact = null;
+        consignForm.consignee_mobile = null;
+      } else {
+        consignForm.consignee_id = null;
+      }
+    }
+    this.handleSetClientForm(consignForm);
+  }
+  handleConsignSelect = (key, value) => {
     const consignForm = {};
     if (key === 'consigner_name') {
       const consign = this.props.formRequires.consignerLocations.find(item => item.node_id === value);
-      consignForm.consigner_name = consign.name;
-      consignForm.consigner_province = consign.province;
-      consignForm.consigner_city = consign.city;
-      consignForm.consigner_district = consign.district;
-      consignForm.consigner_street = consign.street;
-      consignForm.consigner_region_code = consign.region_code;
-      consignForm.consigner_addr = consign.addr;
-      consignForm.consigner_email = consign.email;
-      consignForm.consigner_contact = consign.contact;
-      consignForm.consigner_mobile = consign.mobile;
+      if (consign) {
+        consignForm.consigner_id = consign.node_id;
+        consignForm.consigner_name = consign.name;
+        consignForm.consigner_province = consign.province;
+        consignForm.consigner_city = consign.city;
+        consignForm.consigner_district = consign.district;
+        consignForm.consigner_street = consign.street;
+        consignForm.consigner_region_code = consign.region_code;
+        consignForm.consigner_addr = consign.addr;
+        consignForm.consigner_email = consign.email;
+        consignForm.consigner_contact = consign.contact;
+        consignForm.consigner_mobile = consign.mobile;
+      }
     } else if (key === 'consignee_name') {
       const consign = this.props.formRequires.consigneeLocations.find(item => item.node_id === value);
-      consignForm.consignee_name = consign.name;
-      consignForm.consignee_province = consign.province;
-      consignForm.consignee_city = consign.city;
-      consignForm.consignee_district = consign.district;
-      consignForm.consignee_street = consign.street;
-      consignForm.consignee_region_code = consign.region_code;
-      consignForm.consignee_addr = consign.addr;
-      consignForm.consignee_email = consign.email;
-      consignForm.consignee_contact = consign.contact;
-      consignForm.consignee_mobile = consign.mobile;
+      if (consign) {
+        consignForm.consignee_id = consign.node_id;
+        consignForm.consignee_name = consign.name;
+        consignForm.consignee_province = consign.province;
+        consignForm.consignee_city = consign.city;
+        consignForm.consignee_district = consign.district;
+        consignForm.consignee_street = consign.street;
+        consignForm.consignee_region_code = consign.region_code;
+        consignForm.consignee_addr = consign.addr;
+        consignForm.consignee_email = consign.email;
+        consignForm.consignee_contact = consign.contact;
+        consignForm.consignee_mobile = consign.mobile;
+      }
     }
     this.handleSetClientForm(consignForm);
   }
@@ -156,7 +228,7 @@ export default class TransportForm extends Component {
           <Col sm={24}>
             <FormItem label="发货方">
               <Col span="6" style={{ paddingRight: 8 }}>
-                <Select size="large" combobox value={node.consigner_id}
+                <Select size="large" combobox value={node.consigner_id} optionLabelProp="children"
                   onChange={value => this.handleConsignChange('consigner_name', value)}
                 >
                   {formRequires.consignerLocations.map(dw =>
@@ -201,7 +273,7 @@ export default class TransportForm extends Component {
           <Col sm={24}>
             <FormItem label="收货方">
               <Col span="6" style={{ paddingRight: 8 }}>
-                <Select size="large" combobox value={node.consignee_id}
+                <Select size="large" combobox value={node.consignee_id} optionLabelProp="children"
                   onChange={value => this.handleConsignChange('consignee_name', value)}
                 >
                   {formRequires.consigneeLocations.map(dw =>
