cac1618dcb3e23d396c6338227596834e16fe2fc
jiao.yf <jiao.yf@welogix.cn>
2017-03-30 15:24:46 +0800

    cms:修改企业物料库的操作权限

--- client/apps/cms/classification/tradeitem/panes/copCodesPane.jsx
+++ client/apps/cms/classification/tradeitem/panes/copCodesPane.jsx
@@ -173,7 +173,7 @@ export default class CopCodesPane extends React.Component {
     }
     return (
       <Table size="middle" pagination={false} columns={columns} dataSource={this.state.datas}
-        footer={() => repo.permission === CMS_TRADE_REPO_PERMISSION.edit ? <Button type="dashed" onClick={this.handleAdd} icon="plus" style={{ width: '100%' }}>{this.msg('add')}</Button> : null}
+        footer={repo.permission === CMS_TRADE_REPO_PERMISSION.edit ? () => <Button type="dashed" onClick={this.handleAdd} icon="plus" style={{ width: '100%' }}>{this.msg('add')}</Button> : null}
       />
     );
   }
