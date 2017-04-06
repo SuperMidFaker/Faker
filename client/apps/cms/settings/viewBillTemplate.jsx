2e8ac1e1b89ca7f172e9a59513afd5cf7bd1b08c
jiao.yf <jiao.yf@welogix.cn>
2017-04-06 15:36:55 +0800

    cms:add view function to billTemplate for template users who only has the permission view

---
+++ client/apps/cms/settings/viewBillTemplate.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import connectFetch from 'client/common/decorators/connect-fetch';
+import connectNav from 'client/common/decorators/connect-nav';
+import { loadTemplateFormVals } from 'common/reducers/cmsSettings';
+import { loadCmsParams } from 'common/reducers/cmsManifest';
+import BillTemplate from './billTemplate';
+
+function fetchData({ dispatch, state, params }) {
+  const promises = [];
+  promises.push(dispatch(loadCmsParams({
+    ieType: state.cmsSettings.template.ieType,
+    tenantId: state.account.tenantId,
+  })));
+  promises.push(dispatch(loadTemplateFormVals(params.id)));
+  return Promise.all(promises);
+}
+
+@connectFetch()(fetchData)
+@connectNav({
+  depth: 3,
+  moduleName: 'clearance',
+})
+export default class ViewBillTemplate extends Component {
+  render() {
+    return (<BillTemplate operation="view" />);
+  }
+}
