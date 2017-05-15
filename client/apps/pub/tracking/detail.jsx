39de4801ff46b1fd5a7616003a30d96810f0d4c1
jiao.yf <jiao.yf@welogix.cn>
2017-05-15 17:30:25 +0800

    publickShipmentDetail: Changed Alert style.

--- client/apps/pub/tracking/detail.jsx
+++ client/apps/pub/tracking/detail.jsx
@@ -284,8 +284,7 @@ export default class TrackingDetail extends React.Component {
                 <Steps direction={this.state.stepsDirection} current={statusPos}>{steps}</Steps>
                 {deliverDelayException &&
                   <Alert
-                    message="送货超时"
-                    description={`承诺送货时间:${moment(shipmt.pickup_act_date).format('YYYY-MM-DD')}`}
+                    description={`承诺送货时间:${moment(shipmt.deliver_prm_date).format('YYYY-MM-DD')}, 原因：${deliverDelayException.solution}`}
                     type="warning"
                     showIcon
                   />}
