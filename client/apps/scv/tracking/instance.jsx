b4e6674128cea72d4fe72f4dc732c093de9bef32
jiao.yf <jiao.yf@welogix.cn>
2017-05-18 12:18:09 +0800

    scv: Export tracking to excel.

--- client/apps/scv/tracking/instance.jsx
+++ client/apps/scv/tracking/instance.jsx
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import { intlShape, injectIntl } from 'react-intl';
 import moment from 'moment';
 import connectNav from 'client/common/decorators/connect-nav';
-import { Breadcrumb, Layout } from 'antd';
+import { Breadcrumb, Layout, Button } from 'antd';
 import EditableCell from 'client/components/EditableCell';
 import Table from 'client/components/remoteAntTable';
 import { loadTrackingItems, loadTrackingOrders, upsertTrackingOrderCustom } from 'common/reducers/scvTracking';
+import { makeExcel } from 'common/reducers/common';
+import { createFilename } from 'client/util/dataTransform';
 import { format } from 'client/common/i18n/helpers';
 import messages from './message.i18n';
 
@@ -21,7 +23,7 @@ const { Header, Content } = Layout;
     trackingItems: state.scvTracking.trackingItems,
     orders: state.scvTracking.orderList,
   }),
-  { loadTrackingItems, loadTrackingOrders, upsertTrackingOrderCustom }
+  { loadTrackingItems, loadTrackingOrders, upsertTrackingOrderCustom, makeExcel }
 )
 @connectNav({
   depth: 2,
@@ -35,6 +37,7 @@ export default class Instance extends Component {
     loadTrackingItems: PropTypes.func.isRequired,
     loadTrackingOrders: PropTypes.func.isRequired,
     upsertTrackingOrderCustom: PropTypes.func.isRequired,
+    makeExcel: PropTypes.func.isRequired,
   }
   state = {
     tracking: {},
@@ -62,6 +65,42 @@ export default class Instance extends Component {
   handleSave = (id, field, value, source) => {
     this.props.upsertTrackingOrderCustom(id, field, value, source);
   }
+  columns = this.props.trackingItems.map(item => ({
+    key: item.field,
+    dataIndex: item.field,
+    title: item.custom_title,
+    width: item.width,
+    render: (fld, row) => {
+      if (item.editable === 1) {
+        if (item.datatype === 'DATE') {
+          return (
+            <EditableCell value={fld} type="date" cellTrigger
+              onSave={value => this.handleSave(row.id, item.field, value, item.source)}
+            />
+          );
+        } else {
+          return (
+            <EditableCell value={fld} cellTrigger
+              onSave={value => this.handleSave(row.id, item.field, value, item.source)}
+            />
+          );
+        }
+      } else if (item.datatype === 'DATE') {
+        return fld && moment(fld).format('YYYY.MM.DD');
+      } else {
+        return fld;
+      }
+    },
+    renderExcelCell: (fld) => {
+      if (item.source === 3) {
+        return fld;
+      } else if (item.datatype === 'DATE') {
+        return fld ? moment(fld).format('YYYY-MM-DD') : '';
+      } else {
+        return fld;
+      }
+    },
+  }))
   dataSource = new Table.DataSource({
     fetcher: params => this.props.loadTrackingOrders({
       tracking_id: this.props.params.trackingId,
@@ -90,36 +129,28 @@ export default class Instance extends Component {
     },
     remotes: this.props.orders,
   })
+  handleExport = () => {
+    this.props.loadTrackingOrders({
+      tracking_id: this.props.params.trackingId,
+      pageSize: 99999999,
+      current: 1,
+    }).then((result) => {
+      const table = [];
+      const head = this.columns.map(item => item.title);
+      table.push(head);
+      result.data.data.forEach((items) => {
+        const row = this.columns.map(item => item.renderExcelCell(items[item.dataIndex], items));
+        table.push(row);
+      });
+      const sheets = [{ name: 'sheet1', data: table }];
+      this.props.makeExcel(sheets, `${createFilename('scvTracking')}.xlsx`).then((result1) => {
+        window.open(`${API_ROOTS.default}v1/common/excel/${result1.data.filename}`);
+      });
+    });
+  }
   render() {
     const { trackingItems, orders } = this.props;
     const { tracking } = this.state;
-    const columns = trackingItems.map(item => ({
-      key: item.field,
-      dataIndex: item.field,
-      title: item.custom_title,
-      width: item.width,
-      render: (fld, row) => {
-        if (item.editable === 1) {
-          if (item.datatype === 'DATE') {
-            return (
-              <EditableCell value={fld} type="date" cellTrigger
-                onSave={value => this.handleSave(row.id, item.field, value, row.source)}
-              />
-            );
-          } else {
-            return (
-              <EditableCell value={fld} cellTrigger
-                onSave={value => this.handleSave(row.id, item.field, value, row.source)}
-              />
-            );
-          }
-        } else if (item.datatype === 'DATE') {
-          return fld && moment(fld).format('YYYY.MM.DD');
-        } else {
-          return fld;
-        }
-      },
-    }));
     this.dataSource.remotes = orders;
     const tableWidth = trackingItems.map(item => item.width).reduce((a, b) => a + b, 0);
     return (
@@ -133,11 +164,14 @@ export default class Instance extends Component {
               {tracking.name}
             </Breadcrumb.Item>
           </Breadcrumb>
+          <div className="top-bar-tools">
+            <Button type="primary" size="large" ghost icon="export" onClick={this.handleExport}>导出</Button>
+          </div>
         </Header>
         <Content className="main-content" key="main">
           <div className="page-body">
             <div className="panel-body table-panel">
-              <Table columns={columns} scroll={{ x: tableWidth }} dataSource={this.dataSource} rowKey="shipmt_order_no" />
+              <Table columns={this.columns} scroll={{ x: tableWidth }} dataSource={this.dataSource} rowKey="shipmt_order_no" />
             </div>
           </div>
         </Content>
