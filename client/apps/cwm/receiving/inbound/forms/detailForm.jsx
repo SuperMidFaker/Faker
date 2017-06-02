af1f1e684bccfb9ba53510f63011d248144c8114
Eric Shao <eric.shao@welogix.cn>
2017-06-02 14:05:23 +0800

    Updated receive UI prototype

--- client/apps/cwm/receiving/inbound/forms/detailForm.jsx
+++ client/apps/cwm/receiving/inbound/forms/detailForm.jsx
@@ -1,6 +1,6 @@
 /* eslint react/no-multi-comp: 0 */
 import React, { Component, PropTypes } from 'react';
-import { Button, Card, Table } from 'antd';
+import { Button, Card, Input, Table, Tag, Tooltip } from 'antd';
 import { intlShape, injectIntl } from 'react-intl';
 import { format } from 'client/common/i18n/helpers';
 import RowUpdater from 'client/components/rowUpdater';
@@ -30,8 +30,9 @@ export default class DetailForm extends Component {
   }, {
     title: '订单数量',
     dataIndex: 'order_qty',
+    render: o => (<b>{o}</b>),
   }, {
-    title: '单位',
+    title: '主单位',
     dataIndex: 'unit',
   }, {
     title: '包装代码',
@@ -39,12 +40,22 @@ export default class DetailForm extends Component {
   }, {
     title: '收货包装',
     dataIndex: 'receive_pack',
+    render: o => (<Tooltip title="=10主单位" placement="right"><Tag>{o}</Tag></Tooltip>),
   }, {
     title: '预期数量',
-    dataIndex: 'expect_pack_qty',
+    render: (o, record) => (<span><Tooltip title="包装单位数量"><Input className="readonly" value={record.expect_pack_qty} style={{ width: 80 }} /></Tooltip>
+      <Tooltip title="主单位数量"><Input value={record.expect_qty} style={{ width: 80 }} disabled /></Tooltip></span>),
   }, {
-    title: '已收数量',
-    dataIndex: 'received_pack_qty',
+    title: '收货数量',
+    render: (o, record) => {
+      if (record.expect_pack_qty === record.received_pack_qty) {
+        return (<span className="mdc-text-green"><Tooltip title="包装单位数量"><Input className="readonly" value={record.received_pack_qty} style={{ width: 80 }} /></Tooltip>
+          <Tooltip title="主单位数量"><Input value={record.received_qty} style={{ width: 80 }} disabled /></Tooltip></span>);
+      } else {
+        return (<span className="mdc-text-red"><Tooltip title="包装单位数量"><Input className="readonly" value={record.received_pack_qty} style={{ width: 80 }} /></Tooltip>
+          <Tooltip title="主单位数量"><Input value={record.received_qty} style={{ width: 80 }} disabled /></Tooltip></span>);
+      }
+    },
   }, {
     title: '操作',
     render: (o, record) => {
@@ -60,48 +71,63 @@ export default class DetailForm extends Component {
   dataSource = [{
     seq_no: '1',
     product_no: 'N04601170548',
-    order_qty: 1,
-    desc_cn: '0961|希雅路仓库',
+    order_qty: 15,
+    desc_cn: '微纤维止血胶原粉',
     packing_code: 'STANDARD',
     unit: '件',
-    expect_pack_qty: 0,
-    receive_pack: '主单位',
+    receive_pack: '单件',
+    expect_pack_qty: 15,
+    expect_qty: 15,
+    received_pack_qty: 15,
+    received_qty: 15,
   }, {
     seq_no: '2',
     product_no: 'N04601170547',
-    order_qty: 0,
-    desc_cn: '0086|物流大道仓库',
+    order_qty: 1000,
+    desc_cn: 'PTA球囊扩张导管',
     packing_code: 'STANDARD',
     unit: '件',
-    expect_pack_qty: 1,
     receive_pack: '内包装',
+    expect_pack_qty: 10,
+    expect_qty: 1000,
+    received_pack_qty: 0,
+    received_qty: 0,
   }, {
     seq_no: '3',
     product_no: 'N04601170546',
-    order_qty: 1,
-    desc_cn: '0962|富特路仓库',
+    order_qty: 1000,
+    desc_cn: '临时起搏电极导管',
     packing_code: 'STANDARD',
     unit: '个',
-    expect_pack_qty: 2,
     receive_pack: '内包装',
+    expect_pack_qty: 10,
+    expect_qty: 1000,
+    received_pack_qty: 0,
+    received_qty: 0,
   }, {
     seq_no: '4',
     product_no: 'N04601170546',
-    order_qty: 1,
-    desc_cn: '0962|富特路仓库',
+    order_qty: 12,
+    desc_cn: '肾造瘘球囊扩张导管',
     packing_code: 'STANDARD',
     unit: '个',
     expect_pack_qty: 2,
     receive_pack: '箱',
+    expect_qty: 12,
+    received_pack_qty: 1,
+    received_qty: 6,
   }, {
     seq_no: '5',
     product_no: 'N04601170546',
     order_qty: 1,
-    desc_cn: '0962|富特路仓库',
+    desc_cn: '输尿管镜球囊扩张导管',
     packing_code: 'STANDARD',
     unit: '个',
-    expect_pack_qty: 3,
+    expect_pack_qty: 1,
     receive_pack: '托盘',
+    expect_qty: 1,
+    received_pack_qty: 0,
+    received_qty: 0,
   }];
   render() {
     return (
--- client/apps/cwm/receiving/inbound/forms/headForm.jsx
+++ client/apps/cwm/receiving/inbound/forms/headForm.jsx
@@ -1,12 +1,13 @@
 /* eslint react/no-multi-comp: 0 */
 import React, { Component, PropTypes } from 'react';
-import { Form, Input, Progress, Card, Col, Row } from 'antd';
+import { Progress, Card, Col, Icon, Menu, Row, Tag } from 'antd';
 import { intlShape, injectIntl } from 'react-intl';
+import Avatar from 'react-avatar';
+import InfoItem from 'client/components/InfoItem';
 import { format } from 'client/common/i18n/helpers';
 import messages from '../../message.i18n';
 
 const formatMsg = format(messages);
-const FormItem = Form.Item;
 
 @injectIntl
 export default class HeadForm extends Component {
@@ -38,49 +39,28 @@ export default class HeadForm extends Component {
     dataIndex: 'remark',
   }]
   render() {
-    const { form: { getFieldDecorator } } = this.props;
     return (
       <Card>
         <Row gutter={16}>
-          <Col sm={24} lg={6}>
-            <FormItem label="入库单号">
-              {getFieldDecorator('inbound_no', {
-              })(
-                <Input disabled />
-                  )}
-            </FormItem>
+          <Col sm={24} lg={8}>
+            <InfoItem label="入库单号" field="I096120170603223-01" />
           </Col>
-          <Col sm={24} lg={6}>
-            <FormItem label="入库状态">
-              {getFieldDecorator('inbound_status', {
-              })(
-                <Input disabled />
-                  )}
-            </FormItem>
+          <Col sm={24} lg={4}>
+            <InfoItem label="入库状态" field={<Tag color="blue">上架</Tag>} />
           </Col>
-          <Col sm={24} lg={6}>
-            <FormItem label="操作人员">
-              {getFieldDecorator('inbound_type', {
-              })(
-                <Input />
-                  )}
-            </FormItem>
+          <Col sm={24} lg={4}>
+            <InfoItem label="预计箱数" addonBefore={<Icon type="inbox" />} field={10} />
           </Col>
-          <Col sm={24} lg={3}>
-            <FormItem label="预计箱数">
-              {getFieldDecorator('convey_box_qty', {
-              })(
-                <Input />
-                  )}
-            </FormItem>
+          <Col sm={24} lg={4}>
+            <InfoItem label="预计托盘数" addonBefore={<Icon type="appstore-o" />} field={2} />
           </Col>
-          <Col sm={24} lg={3}>
-            <FormItem label="预计托盘数">
-              {getFieldDecorator('convey_pallet_qty', {
-              })(
-                <Input />
-                  )}
-            </FormItem>
+          <Col sm={24} lg={4}>
+            <InfoItem type="dropdown" label="操作人员" addonBefore={<Avatar name="未分配" size={28} round />}
+              placeholder="分配操作人员" editable
+              overlay={<Menu onClick={this.handleMenuClick}>
+                <Menu.Item key={1}>仓管员</Menu.Item>
+              </Menu>}
+            />
           </Col>
           <Col sm={24} lg={24}>
             <Progress percent={50} status="active" />
