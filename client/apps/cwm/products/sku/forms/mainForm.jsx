5af631623dde530d353801d750445a57b64ad589
Eric Shao <eric.shao@welogix.cn>
2017-06-05 13:33:47 +0800

    Updated SKU creation UI

--- client/apps/cwm/products/sku/forms/mainForm.jsx
+++ client/apps/cwm/products/sku/forms/mainForm.jsx
@@ -1,6 +1,6 @@
 /* eslint react/no-multi-comp: 0 */
 import React, { Component, PropTypes } from 'react';
-import { Form, Select, Card, Col, Icon, Input, Row, Tooltip } from 'antd';
+import { Dropdown, Menu, Form, Select, Card, Col, Icon, Input, Row, Tooltip, Checkbox } from 'antd';
 import { intlShape, injectIntl } from 'react-intl';
 import { format } from 'client/common/i18n/helpers';
 import messages from '../../message.i18n';
@@ -41,6 +41,12 @@ export default class MainForm extends Component {
   }]
   render() {
     const { form: { getFieldDecorator } } = this.props;
+    const variantsMenu = (
+      <Menu>
+        <Menu.Item key="1">SKU1</Menu.Item>
+        <Menu.Item key="2">SKU2</Menu.Item>
+      </Menu>
+    );
     return (
       <div>
         <Card title="产品属性">
@@ -102,17 +108,17 @@ export default class MainForm extends Component {
               </FormItem>
             </Col>
             <Col sm={24} lg={12}>
-              <FormItem label={this.msg('unitMeasure')}>
+              <FormItem label={this.msg('measureUnit')}>
                 {getFieldDecorator('unit', {
                 })(
                   <Select
                     optionFilterProp="search"
                     placeholder="选择计量主单位"
                   >
-                    <Option value="Ballo">个</Option>
-                    <Option value="CPU">件</Option>
-                    <Option value="CPU">套</Option>
-                    <Option value="Float">公斤</Option>
+                    <Option value="001">个</Option>
+                    <Option value="002">件</Option>
+                    <Option value="003">套</Option>
+                    <Option value="004">公斤</Option>
                   </Select>
                   )}
               </FormItem>
@@ -149,7 +155,11 @@ export default class MainForm extends Component {
             </Col>
           </Row>
         </Card>
-        <Card title="SKU属性" extra={<a href="#">Add variant</a>}>
+        <Card title="SKU属性" extra={<span><Checkbox >设置当前为默认</Checkbox>
+          <Dropdown.Button overlay={variantsMenu}>
+            添加变种
+          </Dropdown.Button></span>}
+        >
           <Row gutter={16}>
             <Col sm={24} lg={8}>
               <FormItem label={(
@@ -166,31 +176,32 @@ export default class MainForm extends Component {
               </FormItem>
             </Col>
             <Col sm={24} lg={8}>
-              <FormItem label={this.msg('unit')}>
-                {getFieldDecorator('unit', {
+              <FormItem label={this.msg('skuUnit')}>
+                {getFieldDecorator('sku_unit', {
                 })(
                   <Select
                     optionFilterProp="search"
                     placeholder="选择SKU包装单位"
                   >
-                    <Option value="Float">包</Option>
-                    <Option value="Ballo">木箱</Option>
-                    <Option value="CPU">纸箱</Option>
-                    <Option value="Float">托盘</Option>
+                    <Option value="00">单件(散装)</Option>
+                    <Option value="01">包</Option>
+                    <Option value="02">木箱</Option>
+                    <Option value="03">纸箱</Option>
+                    <Option value="04">托盘</Option>
                   </Select>
                   )}
               </FormItem>
             </Col>
             <Col sm={24} lg={8}>
-              <FormItem label={this.msg('perUnitQty')}>
-                {getFieldDecorator('per_unit_qty', {
+              <FormItem label={this.msg('perSKUQty')}>
+                {getFieldDecorator('per_sku_qty', {
                 })(<InputGroup compact>
                   <Input style={{ width: '50%' }} />
-                  <Select size="large" style={{ width: '50%' }} disabled>
-                    <Option value="Ballo">个</Option>
-                    <Option value="CPU">件</Option>
-                    <Option value="CPU">套</Option>
-                    <Option value="Float">公斤</Option>
+                  <Select defaultValue="001" size="large" style={{ width: '50%' }} disabled>
+                    <Option value="001">个</Option>
+                    <Option value="002">件</Option>
+                    <Option value="003">套</Option>
+                    <Option value="004">公斤</Option>
                   </Select>
                 </InputGroup>)}
               </FormItem>
