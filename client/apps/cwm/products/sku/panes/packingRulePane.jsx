12ebf30f3a1d173a5c943f10b0cc34e83c148990
zhaochenchen <ialwayswannafly@yahoo.com>
2017-06-23 17:05:59 +0800

    loadSkuTemplates && Stripe component

--- client/apps/cwm/products/sku/panes/packingRulePane.jsx
+++ client/apps/cwm/products/sku/panes/packingRulePane.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { intlShape, injectIntl } from 'react-intl';
 import { Button, Table } from 'antd';
-import { openPackingRuleModal } from 'common/reducers/cwmSku';
+import { openPackingRuleModal, loadSkuParams, cleanSkuForm } from 'common/reducers/cwmSku';
 import { format } from 'client/common/i18n/helpers';
 import PackingRuleModal from '../modal/packingRuleModal';
 import messages from '../../message.i18n';
@@ -15,8 +15,10 @@ const formatMsg = format(messages);
   state => ({
     tenantId: state.account.tenantId,
     tenantName: state.account.tenantName,
+    owner: state.cwmSku.owner,
+    packings: state.cwmSku.params.packings,
   }),
-  { openPackingRuleModal }
+  { openPackingRuleModal, loadSkuParams, cleanSkuForm }
 )
 export default class PackingRulePane extends React.Component {
   static propTypes = {
@@ -28,7 +30,17 @@ export default class PackingRulePane extends React.Component {
     datas: [],
     brokers: [],
   };
-
+  componentWillMount() {
+    this.props.cleanSkuForm();
+    if (this.props.owner.id) {
+      this.props.loadSkuParams(this.props.owner.id);
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.owner.id !== this.props.owner.id) {
+      nextProps.loadSkuParams(nextProps.owner.id);
+    }
+  }
   msg = (descriptor, values) => formatMsg(this.props.intl, descriptor, values)
   handleAddPackingRule = () => {
     this.props.openPackingRuleModal();
@@ -42,7 +54,7 @@ export default class PackingRulePane extends React.Component {
   render() {
     const columns = [{
       title: this.msg('packingCode'),
-      dataIndex: 'packing_code',
+      dataIndex: 'code',
       width: 120,
     }, {
       title: this.msg('desc'),
@@ -50,7 +62,7 @@ export default class PackingRulePane extends React.Component {
     }];
     return (
       <div>
-        <Table size="middle" pagination={false} columns={columns} dataSource={this.state.datas} rowKey="id"
+        <Table size="middle" pagination={false} columns={columns} dataSource={this.props.packings} rowKey="id"
           footer={() => <Button type="dashed" onClick={this.handleAddPackingRule} icon="plus" style={{ width: '100%' }}>{this.msg('add')}</Button>}
         />
         <PackingRuleModal />
