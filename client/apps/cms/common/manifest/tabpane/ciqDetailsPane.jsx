19335e3b94ff934af5f1a3a068444211f3128d06
vicki <776128986@qq.com>
2017-07-10 10:46:07 +0800

    modify ciqDetailsPane

--- client/apps/cms/common/manifest/tabpane/ciqDetailsPane.jsx
+++ client/apps/cms/common/manifest/tabpane/ciqDetailsPane.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Table } from 'antd';
 import { intlShape, injectIntl } from 'react-intl';
@@ -8,7 +9,26 @@ const formatMsg = format(messages);
 import { buildTipItems } from 'client/common/customs';
 
 @injectIntl
-
+@connect(
+  state => ({
+    units: state.cmsManifest.params.units.map(un => ({
+      value: un.unit_code,
+      text: un.unit_name,
+    })),
+    countries: state.cmsManifest.params.tradeCountries.map(tc => ({
+      value: tc.cntry_co,
+      text: tc.cntry_name_cn,
+    })),
+    currencies: state.cmsManifest.params.currencies.map(cr => ({
+      value: cr.curr_code,
+      text: cr.curr_name,
+    })),
+    exemptions: state.cmsManifest.params.exemptionWays.map(ep => ({
+      value: ep.value,
+      text: ep.text,
+    })),
+  })
+)
 export default class ManifestLegalInspection extends React.Component {
   static propTypes = {
     intl: intlShape.isRequired,
@@ -27,16 +47,10 @@ export default class ManifestLegalInspection extends React.Component {
       fixed: 'left',
       width: 150,
       dataIndex: 'cop_g_no',
-    /*
-    }, {
-      title: this.msg('emGNo'),
-      width: 100,
-      dataIndex: 'em_g_no',
-    */
     }, {
       title: this.msg('codeT'),
       width: 110,
-      dataIndex: 'code_t',
+      dataIndex: 'codes',
     }, {
       title: this.msg('gName'),
       width: 200,
@@ -65,6 +79,11 @@ export default class ManifestLegalInspection extends React.Component {
       width: 80,
       className: 'cell-align-right',
       dataIndex: 'g_unit',
+      render: (o) => {
+        const unit = this.props.units.filter(cur => cur.value === o)[0];
+        const text = unit ? `${unit.value}| ${unit.text}` : o;
+        return text;
+      },
     }, {
       title: <div className="cell-align-right">{this.msg('decPrice')}</div>,
       width: 100,
@@ -79,6 +98,11 @@ export default class ManifestLegalInspection extends React.Component {
       title: this.msg('currency'),
       width: 100,
       dataIndex: 'trade_curr',
+      render: (o) => {
+        const currency = this.props.currencies.filter(cur => cur.value === o)[0];
+        const text = currency ? `${currency.value}| ${currency.text}` : o;
+        return text;
+      },
     }, {
       title: <div className="cell-align-right">{this.msg('grosswt')}</div>,
       width: 80,
@@ -98,6 +122,11 @@ export default class ManifestLegalInspection extends React.Component {
       title: this.msg('unit1'),
       width: 80,
       dataIndex: 'unit_1',
+      render: (o) => {
+        const unit = this.props.units.filter(cur => cur.value === o)[0];
+        const text = unit ? `${unit.value}| ${unit.text}` : o;
+        return text;
+      },
     }, {
       title: <div className="cell-align-right">{this.msg('qty2')}</div>,
       width: 80,
@@ -107,43 +136,39 @@ export default class ManifestLegalInspection extends React.Component {
       title: this.msg('unit2'),
       width: 80,
       dataIndex: 'unit_2',
+      render: (o) => {
+        const unit = this.props.units.filter(cur => cur.value === o)[0];
+        const text = unit ? `${unit.value}| ${unit.text}` : o;
+        return text;
+      },
     }, {
       title: this.msg('exemptionWay'),
       width: 80,
       dataIndex: 'duty_mode',
+      render: (o) => {
+        const exemption = this.props.exemptions.filter(cur => cur.value === o)[0];
+        const text = exemption ? `${exemption.value}| ${exemption.text}` : o;
+        return text;
+      },
     }, {
       title: this.msg('ecountry'),
       width: 120,
       dataIndex: 'dest_country',
+      render: (o) => {
+        const country = this.props.countries.filter(cur => cur.value === o)[0];
+        const text = country ? `${country.value}| ${country.text}` : o;
+        return text;
+      },
     }, {
       title: this.msg('icountry'),
       width: 120,
       dataIndex: 'orig_country',
+      render: (o) => {
+        const country = this.props.countries.filter(cur => cur.value === o)[0];
+        const text = country ? `${country.value}| ${country.text}` : o;
+        return text;
+      },
     }];
-    /*
-    {
-      title: this.msg('qtyPcs'),
-      width: 100,
-      dataIndex: 'qty_pcs',
-    }, {
-      title: this.msg('unitPcs'),
-      width: 100,
-      dataIndex: 'unit_pcs',
-    }, {
-      title: this.msg('element'),
-      width: 380,
-      dataIndex: 'element',
-    }, {
-      title: this.msg('versionNo'),
-      width: 80,
-      dataIndex: 'version_no',
-    }, {
-      title: <div className="cell-align-right">{this.msg('processingFees')}</div>,
-      width: 80,
-      className: 'cell-align-right',
-      dataIndex: 'processing_fees',
-    }
-    ];*/
     return (
       <div className="panel-body table-panel">
         <Table columns={columns} dataSource={filterProducts}
