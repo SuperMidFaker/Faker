cd9e8ac29cd5c7a1ecfcddb87e9c878ea2fc43a0
zhaochenchen <ialwayswannafly@yahoo.com>
2017-07-12 18:07:36 +0800

    cwmDashboard

---
+++ common/reducers/cwmDashboard.js
@@ -0,0 +1,49 @@
+import { CLIENT_API } from 'common/reduxMiddlewares/requester';
+import { createActionTypes } from 'client/common/redux-actions';
+
+const actionTypes = createActionTypes('@@welogix/cwm/dashboard/', [
+  'LOAD_STATS', 'LOAD_STATS_SUCCEED', 'LOAD_STATS_FAIL',
+]);
+
+const initState = {
+  statsCard: {
+    inbounds: 0,
+    creates: 0,
+    received: 0,
+    outbounds: 0,
+    unallocated: 0,
+    allocated: 0,
+    picked: 0,
+    shipped: 0,
+    inboundProducts: 0,
+    receipts: 0,
+    putaways: 0,
+    outboundDetails: 0,
+    pickings: 0,
+    shipments: 0,
+  },
+};
+
+export default function reducer(state = initState, action) {
+  switch (action.type) {
+    case actionTypes.LOAD_STATS_SUCCEED:
+      return { ...state, statsCard: { ...action.result.data } };
+    default:
+      return state;
+  }
+}
+
+export function loadStatsCard(startDate, endDate) {
+  return {
+    [CLIENT_API]: {
+      types: [
+        actionTypes.LOAD_STATS,
+        actionTypes.LOAD_STATS_SUCCEED,
+        actionTypes.LOAD_STATS_FAIL,
+      ],
+      endpoint: 'v1/cwm/stats',
+      method: 'get',
+      params: { startDate, endDate },
+    },
+  };
+}
