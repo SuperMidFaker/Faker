dbe871d6a3b69e7043edc41d6c50ad40da48f31f
jiao.yf <jiao.yf@welogix.cn>
2017-07-24 18:07:46 +0800

    remove easemob.

--- server/web/htmlRender.js
+++ server/web/htmlRender.js
@@ -45,8 +45,6 @@ function renderAsHtml(pageCss, pageJs, content) {
 <body>
   <div id="mount" class="full-container">${content}</div>
   <script src="${__CDN__}/assets/lib/jquery/jquery.min.js" type="text/javascript"></script>
-  <script src="${__CDN__}/assets/lib/easemob/strophe.js" type="text/javascript"></script>
-  <script src="${__CDN__}/assets/lib/easemob/websdk-1.1.2.js" type="text/javascript"></script>
   <script src="https://a.alipayobjects.com/g/datavis/g6/0.2.2/index.js"></script>
   ${pageJs}
 </body>
