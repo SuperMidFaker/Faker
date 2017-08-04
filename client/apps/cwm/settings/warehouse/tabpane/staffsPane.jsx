5db9e94f334a7cc11c580fc229edde39d0ee80ad
zhaochenchen <ialwayswannafly@yahoo.com>
2017-08-04 11:23:27 +0800

    staff modal

--- client/apps/cwm/settings/warehouse/tabpane/staffsPane.jsx
+++ client/apps/cwm/settings/warehouse/tabpane/staffsPane.jsx
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { intlShape, injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
-import moment from 'moment';
-import { Button, Layout, Table, Tag } from 'antd';
-import { showWhseOwnersModal, loadwhseOwners, showOwnerControlModal, changeOwnerStatus } from 'common/reducers/cwmWarehouse';
-import { loadWhse } from 'common/reducers/cwmContext';
-import RowUpdater from 'client/components/rowUpdater';
-import WhseOwnersModal from '../modal/whseOwnersModal';
-import OwnerControlModal from '../modal/ownerControlModal';
+import { Button, Layout, Table } from 'antd';
+import { showStaffModal, loadStaffs } from 'common/reducers/cwmWarehouse';
+import StaffModal from '../modal/staffModal';
 import { formatMsg } from '../message.i18n';
 
 const { Content } = Layout;
@@ -17,94 +13,48 @@ const { Content } = Layout;
 @connect(
   state => ({
     tenantId: state.account.tenantId,
-    whseOwners: state.cwmWarehouse.whseOwners,
-    defaultWhse: state.cwmContext.defaultWhse,
+    staffs: state.cwmWarehouse.staffs,
   }),
-  { showWhseOwnersModal, loadwhseOwners, showOwnerControlModal, changeOwnerStatus, loadWhse }
+  { showStaffModal, loadStaffs }
 )
 export default class StaffsPane extends Component {
   static propTypes = {
     intl: intlShape.isRequired,
     whseCode: PropTypes.string.isRequired,
     whseTenantId: PropTypes.number.isRequired,
-    whseOwners: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      owner_code: PropTypes.string,
-      owner_name: PropTypes.string.isRequired,
-    })),
   }
   state = {
     selectedRowKeys: [],
   }
   componentWillMount() {
-    this.props.loadwhseOwners(this.props.whseCode, this.props.whseTenantId);
+    this.props.loadStaffs(this.props.whseCode, this.props.tenantId);
   }
   componentWillReceiveProps(nextProps) {
     if (nextProps.whseCode !== this.props.whseCode) {
-      this.props.loadwhseOwners(nextProps.whseCode, nextProps.whseTenantId);
+      this.props.loadStaffs(nextProps.whseCode, nextProps.whseTenantId);
+    }
+    if (nextProps.staffs !== this.props.staffs) {
+      const selectedUserIds = nextProps.staffs.map(item => Number(item.login_id));
+      this.setState({
+        selectedRowKeys: selectedUserIds,
+      });
     }
   }
   columns = [{
     title: '姓名',
     dataIndex: 'staff_name',
     width: 150,
-  }, {
-    title: '状态',
-    dataIndex: 'active',
-    render: (o) => {
-      if (o) {
-        return <Tag color="green">正常</Tag>;
-      } else {
-        return <Tag>停用</Tag>;
-      }
-    },
-  }, {
-    title: '最后修改时间',
-    dataIndex: 'last_updated_date',
-    width: 120,
-    render: o => o && moment(o).format('YYYY.MM.DD HH:mm'),
-  }, {
-    title: '创建时间',
-    dataIndex: 'created_date',
-    width: 120,
-    render: o => o && moment(o).format('YYYY.MM.DD HH:mm'),
-  }, {
-    title: '操作',
-    width: 100,
-    render: record => (
-      <span>
-        {record.active === 0 ? <RowUpdater onHit={() => this.changeOwnerStatus(record.id, true)} label="启用" row={record} /> :
-        <RowUpdater onHit={() => this.changeOwnerStatus(record.id, false)} label="停用" row={record} />}
-      </span>
-    ),
   }]
   msg = formatMsg(this.props.intl)
-  handleOwnerControl = (ownerAuth) => {
-    this.props.showOwnerControlModal(ownerAuth);
-  }
-  changeOwnerStatus = (id, status) => {
-    this.props.changeOwnerStatus(id, status).then((result) => {
-      if (!result.error) {
-        this.handleOwnerLoad();
-      }
-    });
-  }
-  handleOwnerLoad = () => {
-    this.props.loadwhseOwners(this.props.whseCode, this.props.whseTenantId);
-    if (this.props.whseCode === this.props.defaultWhse.code) {
-      this.props.loadWhse(this.props.whseCode, this.props.tenantId);
-    }
-  }
   render() {
-    const { whseCode, whseTenantId, whseOwners } = this.props;
+    const { whseCode, staffs } = this.props;
     return (
       <Content>
         <div className="toolbar">
-          <Button type="primary" ghost icon="plus-circle" onClick={() => this.props.showWhseOwnersModal()}>添加员工</Button>
+          <Button type="primary" ghost icon="plus-circle" onClick={() => this.props.showStaffModal()}>添加员工</Button>
         </div>
-        <Table columns={this.columns} dataSource={whseOwners} rowKey="id" />
-        <WhseOwnersModal whseCode={whseCode} whseTenantId={whseTenantId} whseOwners={whseOwners} />
-        <OwnerControlModal whseCode={whseCode} reload={this.handleOwnerLoad} />
+        <Table columns={this.columns} dataSource={staffs} rowKey="id" />
+        <StaffModal whseCode={whseCode} selectedUserIds={this.state.selectedRowKeys} />
       </Content>
     );
   }
