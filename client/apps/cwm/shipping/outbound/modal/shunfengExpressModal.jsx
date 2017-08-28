d4738bd56b9a971a067a860312ddd46958a32b7e
jiao.yf <jiao.yf@welogix.cn>
2017-08-28 18:19:15 +0800

    cwm: 调整shunfengExpressModal.jsx

--- client/apps/cwm/shipping/outbound/modal/shunfengExpressModal.jsx
+++ client/apps/cwm/shipping/outbound/modal/shunfengExpressModal.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Icon, Tabs, Card, Col, Row, Tooltip, Modal, Form, Input,
+import { Icon, Tabs, Card, Col, Row, Tooltip, Modal, Form, Input, Radio,
   Table, Select } from 'antd';
 import { intlShape, injectIntl } from 'react-intl';
 import { loadOutboundHead, updateOutboundMode, readWaybillLogo, loadCourierNo, toggleShunfengExpressModal } from 'common/reducers/cwmOutbound';
@@ -14,6 +14,8 @@ const formatMsg = format(messages);
 const TabPane = Tabs.TabPane;
 const FormItem = Form.Item;
 const Option = Select.Option;
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
 
 const EXPRESS_TYPES = [
   { text: '顺丰标快', value: '1' },
@@ -71,13 +73,21 @@ export default class ShunfengExpressModal extends Component {
     expressNum: 0,
 
 
-    phone: '15757129719',
-    address: '物流大道浦东物流股份有限公司',
-    contact: '徐天龙',
-    province: '上海市',
-    city: '市辖区',
-    district: '浦东新区',
-    street: '',
+    receiver_phone: '15757129719',
+    receiver_address: '物流大道浦东物流股份有限公司',
+    receiver_contact: '徐天龙',
+    receiver_province: '上海市',
+    receiver_city: '市辖区',
+    receiver_district: '浦东新区',
+    receiver_street: '',
+
+    sender_phone: '15757129719',
+    sender_address: '物流大道浦东物流股份有限公司',
+    sender_contact: '徐天龙',
+    sender_province: '上海市',
+    sender_city: '市辖区',
+    sender_district: '浦东新区',
+    sender_street: '',
 
     express_type: '13',
     pay_method: '1',
@@ -85,6 +95,9 @@ export default class ShunfengExpressModal extends Component {
     weight: 0,
     custid: '',
     product_name: '',
+    product_qty: 0,
+    pack_qty: 0,
+    need_return_tracking_no: '0',
   }
   componentWillMount() {
     this.props.readWaybillLogo();
@@ -189,12 +202,20 @@ export default class ShunfengExpressModal extends Component {
       this.props.loadOutboundHead(this.props.params.outboundNo);
     });
   }
-  handleRegionChange = (value) => {
+  handleReceiverRegionChange = (value) => {
     this.setState({
-      province: value[1],
-      city: value[2],
-      district: value[3],
-      street: value[4],
+      receiver_province: value[1],
+      receiver_city: value[2],
+      receiver_district: value[3],
+      receiver_street: value[4],
+    });
+  }
+  handleSenderRegionChange = (value) => {
+    this.setState({
+      sender_province: value[1],
+      sender_city: value[2],
+      sender_district: value[3],
+      sender_street: value[4],
     });
   }
   render() {
@@ -224,8 +245,10 @@ export default class ShunfengExpressModal extends Component {
       width: 80,
       render: (col, row, index) => (<a onClick={() => this.handleWaybillPrint(courierNo[0], row.courier_no, index + 1)}><Icon type="printer" /></a>),
     }];
-    const { province, city, district, street } = this.state;
-    const regionValues = [province, city, district, street];
+    const receiverRegionValues = [this.state.receiver_province, this.state.receiver_city,
+      this.state.receiver_district, this.state.receiver_street];
+    const senderRegionValues = [this.state.sender_province, this.state.sender_city,
+      this.state.sender_district, this.state.sender_street];
 
     return (
       <Modal title="顺丰快递单打印" visible={this.props.visible} width={700}
@@ -242,7 +265,16 @@ export default class ShunfengExpressModal extends Component {
                   </Select>
                 </FormItem>
               </Col>
-              <Col span={12} />
+              <Col span={12} >
+                <FormItem label="签单返还" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                  <RadioGroup value={this.state.need_return_tracking_no}
+                    onChange={value => this.setState({ need_return_tracking_no: value })} style={{ width: '100%' }}
+                  >
+                    <RadioButton value="0">否</RadioButton>
+                    <RadioButton value="1">是</RadioButton>
+                  </RadioGroup>
+                </FormItem>
+              </Col>
             </Row>
             <Row>
               <Col span={12}>
@@ -275,12 +307,12 @@ export default class ShunfengExpressModal extends Component {
             <Row>
               <Col span={12}>
                 <FormItem label="装箱数量" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-                  <Input value={this.state.insure} type="number" onChange={e => this.setState({ insure: e.target.value })} />
+                  <Input value={this.state.pack_qty} type="number" onChange={e => this.setState({ pack_qty: e.target.value })} />
                 </FormItem>
               </Col>
               <Col span={12}>
                 <FormItem label="托寄物" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-                  <Input value={this.state.product_name} type="number" onChange={e => this.setState({ product_name: e.target.value })} />
+                  <Input value={this.state.product_name} onChange={e => this.setState({ product_name: e.target.value })} />
                 </FormItem>
               </Col>
             </Row>
@@ -292,46 +324,77 @@ export default class ShunfengExpressModal extends Component {
               </Col>
               <Col span={12}>
                 <FormItem label="总件数" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-                  <Input value={this.state.insure} type="number" onChange={e => this.setState({ insure: e.target.value })} />
+                  <Input value={this.state.product_qty} type="number" onChange={e => this.setState({ product_qty: e.target.value })} />
                 </FormItem>
               </Col>
             </Row>
-
-            <Row>
-              <Col span={12}>
-                <FormItem label="收货人" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-                  <Input value={this.state.contact} placeholder="收货人"
-                    onChange={e => this.setState({ contact: e.target.value })}
-                  />
-                </FormItem>
-              </Col>
-              <Col span={12}>
-                <FormItem label="电话" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-                  <Input value={this.state.phone} placeholder="电话"
-                    onChange={e => this.setState({ phone: e.target.value })}
-                  />
-                </FormItem>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>
-                <FormItem label="地址" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-                  <Cascader defaultRegion={regionValues} onChange={this.handleRegionChange} />
-                </FormItem>
-              </Col>
-              <Col span={12}>
-                <FormItem label="详细地址" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-                  <Input value={this.state.address}
-                    onChange={e => this.setState({ address: e.target.value })}
-                  />
-                </FormItem>
-              </Col>
-            </Row>
+            <Card>
+              <Row>
+                <Col span={12}>
+                  <FormItem label="收货人" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input value={this.state.receiver_contact} placeholder="收货人"
+                      onChange={e => this.setState({ receiver_contact: e.target.value })}
+                    />
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="电话" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input value={this.state.receiver_phone} placeholder="电话"
+                      onChange={e => this.setState({ receiver_phone: e.target.value })}
+                    />
+                  </FormItem>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={12}>
+                  <FormItem label="地址" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Cascader defaultRegion={receiverRegionValues} onChange={this.handleReceiverRegionChange} />
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="详细地址" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input value={this.state.receiver_address}
+                      onChange={e => this.setState({ receiver_address: e.target.value })}
+                    />
+                  </FormItem>
+                </Col>
+              </Row>
+            </Card>
+            <Card>
+              <Row>
+                <Col span={12}>
+                  <FormItem label="发货人" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input value={this.state.sender_contact} placeholder="发货人"
+                      onChange={e => this.setState({ sender_contact: e.target.value })}
+                    />
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="电话" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input value={this.state.sender_phone} placeholder="电话"
+                      onChange={e => this.setState({ sender_phone: e.target.value })}
+                    />
+                  </FormItem>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={12}>
+                  <FormItem label="地址" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Cascader defaultRegion={senderRegionValues} onChange={this.handleSenderRegionChange} />
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="详细地址" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input value={this.state.sender_address}
+                      onChange={e => this.setState({ sender_address: e.target.value })}
+                    />
+                  </FormItem>
+                </Col>
+              </Row>
+            </Card>
           </TabPane>
           <TabPane tab="快递单号" key="2">
-            <Card title="快递单号" bodyStyle={{ padding: 0 }}>
-              <Table dataSource={dataSource} columns={columns} showHeader={false} size="small" pagination={false} />
-            </Card>
+            <Table dataSource={dataSource} columns={columns} showHeader={false} size="small" pagination={false} />
           </TabPane>
         </Tabs>
       </Modal>
