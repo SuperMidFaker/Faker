61e12a99f003c05875e5600b2d387d3fb82b2b55
jiao.yf <jiao.yf@welogix.cn>
2017-08-29 16:39:37 +0800

    cwm: 顺丰获取运单号

--- client/apps/cwm/shipping/outbound/modal/shunfengExpressModal.jsx
+++ client/apps/cwm/shipping/outbound/modal/shunfengExpressModal.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Icon, Tabs, Card, Col, Row, Tooltip, Modal, Form, Input, Radio,
+import { Icon, Tabs, Card, Col, Row, Tooltip, Modal, Form, Input, Radio, Button,
   Table, Select } from 'antd';
 import { intlShape, injectIntl } from 'react-intl';
-import { loadOutboundHead, updateOutboundMode, readWaybillLogo, loadCourierNo, toggleShunfengExpressModal } from 'common/reducers/cwmOutbound';
+import { loadOutboundHead, updateOutboundMode, readWaybillLogo, orderExpress, toggleShunfengExpressModal,
+loadExpressInfo, addZD } from 'common/reducers/cwmOutbound';
 import messages from '../../message.i18n';
 import { format } from 'client/common/i18n/helpers';
 import { WaybillDef } from '../billsPrint/docDef';
@@ -32,6 +33,11 @@ const PAY_METHODS = [
   { text: '第三方付', value: '3' },
 ];
 
+const ADDED_SERVICES = [
+  { text: '代收货款', value: 'COD' },
+  { text: '保价', value: 'INSURE' },
+];
+
 @injectIntl
 @connect(
   state => ({
@@ -45,7 +51,7 @@ const PAY_METHODS = [
     outboundProducts: state.cwmOutbound.outboundProducts,
     visible: state.cwmOutbound.shunfengExpressModal.visible,
   }),
-  { loadOutboundHead, updateOutboundMode, readWaybillLogo, loadCourierNo, toggleShunfengExpressModal }
+  { loadOutboundHead, updateOutboundMode, readWaybillLogo, orderExpress, toggleShunfengExpressModal, loadExpressInfo, addZD }
 )
 export default class ShunfengExpressModal extends Component {
   static propTypes = {
@@ -54,6 +60,8 @@ export default class ShunfengExpressModal extends Component {
     updateOutboundMode: PropTypes.func.isRequired,
     toggleShunfengExpressModal: PropTypes.func.isRequired,
     outboundProducts: PropTypes.arrayOf(PropTypes.shape({ seq_no: PropTypes.string.isRequired })),
+    loadExpressInfo: PropTypes.func.isRequired,
+    addZD: PropTypes.func.isRequired,
   }
   static contextTypes = {
     router: PropTypes.object.isRequired,
@@ -67,37 +75,45 @@ export default class ShunfengExpressModal extends Component {
 // 第六单：物流普运--寄付月结
   state = {
     printedPickingList: false,
-    picking: false,
-    picked: false,
-    tabKey: 'orderDetails',
-    expressNum: 0,
+    tabkey: '1',
 
-
-    receiver_phone: '15757129719',
-    receiver_address: '物流大道浦东物流股份有限公司',
-    receiver_contact: '徐天龙',
-    receiver_province: '上海市',
-    receiver_city: '市辖区',
-    receiver_district: '浦东新区',
+    receiver_phone: '',
+    receiver_address: '',
+    receiver_contact: '',
+    receiver_province: '',
+    receiver_city: '',
+    receiver_district: '',
     receiver_street: '',
+    receiver_region_code: null,
 
-    sender_phone: '15757129719',
-    sender_address: '物流大道浦东物流股份有限公司',
-    sender_contact: '徐天龙',
-    sender_province: '上海市',
-    sender_city: '市辖区',
-    sender_district: '浦东新区',
+    sender_phone: '',
+    sender_address: '',
+    sender_contact: '',
+    sender_province: '',
+    sender_city: '',
+    sender_district: '',
     sender_street: '',
+    sender_region_code: null,
 
     express_type: '13',
     pay_method: '1',
-    insure: 0,
+    insure_value: 0,
     weight: 0,
     custid: '',
     product_name: '',
     product_qty: 0,
-    pack_qty: 0,
+    parcel_quantity: 1,
     need_return_tracking_no: '0',
+    added_services: '',
+    cod_value: 0,
+    cod_card_id: '',
+
+    mailno: '',
+    return_tracking_no: '',
+    origincode: '',
+    destcode: '',
+    filter_result: '',
+    remark: '',
   }
   componentWillMount() {
     this.props.readWaybillLogo();
@@ -119,29 +135,94 @@ export default class ShunfengExpressModal extends Component {
       document.body.appendChild(script);
     }
   }
-  msg = key => formatMsg(this.props.intl, key);
+  componentWillReceiveProps(nextProps) {
+    if (!this.props.visible && nextProps.visible) {
+      this.loadExpresseInfo();
+    }
+  }
+  loadExpresseInfo = () => {
+    this.props.loadExpressInfo(this.props.outboundHead.outbound_no).then((result) => {
+      if (result.data) {
+        this.setState({ ...result.data, tabkey: '2' });
+      } else {
+        const { defaultWhse, outboundHead, outboundProducts } = this.props;
+        this.setState({
+          sender_phone: '',
+          sender_address: defaultWhse.whse_address,
+          sender_contact: '',
+          sender_province: defaultWhse.whse_province,
+          sender_city: defaultWhse.whse_city,
+          sender_district: defaultWhse.whse_district,
+          sender_street: defaultWhse.whse_street,
+          sender_region_code: defaultWhse.whse_region_code,
 
+          receiver_phone: outboundHead.receiver_phone || outboundHead.receiver_number,
+          receiver_address: outboundHead.receiver_address,
+          receiver_contact: outboundHead.receiver_contact,
+          receiver_province: outboundHead.receiver_province,
+          receiver_city: outboundHead.receiver_city,
+          receiver_district: outboundHead.receiver_district,
+          receiver_street: outboundHead.receiver_street,
+          receiver_region_code: outboundHead.receiver_region_code,
+
+          product_name: outboundProducts[0] ? outboundProducts[0].name : '',
+        });
+      }
+    });
+  }
+  handleAddZD = () => {
+    const { outboundHead, loginId } = this.props;
+    this.props.addZD({ outboundNo: outboundHead.outbound_no, loginId, expressNum: 1 }).then(() => {
+      this.loadExpresseInfo();
+    });
+  }
+  msg = key => formatMsg(this.props.intl, key)
+  handleTabChange = (tabkey) => {
+    this.setState({ tabkey });
+  }
   handleWaybillPrint = (courierNo, courierNoSon, seq) => {
-    const { outboundHead, outboundProducts } = this.props;
     const expressType = EXPRESS_TYPES.find(item => item.value === this.state.express_type).text;
     const payMethod = PAY_METHODS.find(item => item.value === this.state.pay_method).text;
-    const whseInfo = {
-      phone: this.state.phone,
-      address: this.state.address,
-      contact: this.state.contact,
-      province: this.state.province,
-      city: this.state.city,
-      district: this.state.district,
-      street: this.state.street,
-
+    const expressInfo = {
       express_type: expressType,
       pay_method: payMethod,
-      return_tracking_no: '',
+      insure_value: Number(this.state.insure_value),
+      weight: this.state.weight,
+      custid: this.state.custid,
+      product_name: this.state.product_name,
+      product_qty: this.state.product_qty,
+      parcel_quantity: this.state.parcel_quantity,
+      need_return_tracking_no: this.state.need_return_tracking_no,
+      added_services: this.state.added_services,
+
+      sender_phone: this.state.sender_phone,
+      sender_address: this.state.sender_address,
+      sender_contact: this.state.sender_contact,
+      sender_province: this.state.sender_province,
+      sender_city: this.state.sender_city,
+      sender_district: this.state.sender_district,
+      sender_street: this.state.sender_street,
+      sender_region_code: this.state.sender_region_code,
+
+      receiver_phone: this.state.receiver_phone,
+      receiver_address: this.state.receiver_address,
+      receiver_contact: this.state.receiver_contact,
+      receiver_province: this.state.receiver_province,
+      receiver_city: this.state.receiver_city,
+      receiver_district: this.state.receiver_district,
+      receiver_street: this.state.receiver_street,
+      receiver_region_code: this.state.receiver_region_code,
+
+      mailno: this.state.mailno,
+      return_tracking_no: this.state.return_tracking_no,
+      origincode: this.state.origincode,
+      destcode: this.state.destcode,
+      filter_result: this.state.filter_result,
+      remark: this.state.remark,
     };
     this.setState({
       printedPickingList: true,
     });
-    const { expressNum } = this.state;
     // podWaybillDef
     // const docDefinition = podWaybillDef({
     //   ...this.props.waybill,
@@ -157,11 +238,8 @@ export default class ShunfengExpressModal extends Component {
       ...this.props.waybill,
       courierNo,
       courierNoSon,
-      expressNum: Number(expressNum),
       seq,
-      outboundHead: { ...outboundHead, origincode: '021' },
-      whseInfo,
-      productName: `${outboundProducts[0] ? outboundProducts[0].name : ''} ${outboundHead.total_qty}`,
+      expressInfo,
     });
     window.pdfMake.fonts = {
       selfFont: {
@@ -172,38 +250,50 @@ export default class ShunfengExpressModal extends Component {
     };
     window.pdfMake.createPdf(docDefinition).open();
   }
-  handleTabChange = (tabKey) => {
-    this.setState({
-      tabKey,
-    });
-  }
-  loadCourierNo = () => {
-    const { outboundHead, outboundProducts } = this.props;
+  orderExpress = () => {
+    const { outboundHead } = this.props;
     const expressInfo = {
-      phone: this.state.phone,
-      address: this.state.address,
-      contact: this.state.contact,
-      province: this.state.province,
-      city: this.state.city,
-      district: this.state.district,
-      street: this.state.street,
-      whse_name: this.props.defaultWhse.name,
       express_type: this.state.express_type,
       pay_method: this.state.pay_method,
-      productName: `${outboundProducts[0] ? outboundProducts[0].name : ''} ${outboundHead.total_qty}`,
+      insure_value: Number(this.state.insure_value),
+      weight: this.state.weight,
+      custid: this.state.custid,
+      product_name: this.state.product_name,
+      product_qty: this.state.product_qty,
+      parcel_quantity: this.state.parcel_quantity,
+      need_return_tracking_no: this.state.need_return_tracking_no,
+      added_services: this.state.added_services,
+
+      sender_phone: this.state.sender_phone,
+      sender_address: this.state.sender_address,
+      sender_contact: this.state.sender_contact,
+      sender_province: this.state.sender_province,
+      sender_city: this.state.sender_city,
+      sender_district: this.state.sender_district,
+      sender_street: this.state.sender_street,
+      sender_region_code: this.state.sender_region_code,
+
+      receiver_phone: this.state.receiver_phone,
+      receiver_address: this.state.receiver_address,
+      receiver_contact: this.state.receiver_contact,
+      receiver_province: this.state.receiver_province,
+      receiver_city: this.state.receiver_city,
+      receiver_district: this.state.receiver_district,
+      receiver_street: this.state.receiver_street,
+      receiver_region_code: this.state.receiver_region_code,
     };
-    this.props.loadCourierNo({
-      soNo: this.props.outboundHead.so_no,
-      outboundNo: this.props.params.outboundNo,
+    this.props.orderExpress({
+      soNo: outboundHead.so_no,
+      outboundNo: outboundHead.outbound_no,
       tenantId: this.props.tenantId,
-      expressNum: Number(this.state.expressNum),
       expressInfo,
     }).then(() => {
-      this.props.loadOutboundHead(this.props.params.outboundNo);
+      this.loadExpresseInfo();
     });
   }
   handleReceiverRegionChange = (value) => {
     this.setState({
+      receiver_region_code: value[0],
       receiver_province: value[1],
       receiver_city: value[2],
       receiver_district: value[3],
@@ -212,6 +302,7 @@ export default class ShunfengExpressModal extends Component {
   }
   handleSenderRegionChange = (value) => {
     this.setState({
+      sender_region_code: value[0],
       sender_province: value[1],
       sender_city: value[2],
       sender_district: value[3],
@@ -219,8 +310,8 @@ export default class ShunfengExpressModal extends Component {
     });
   }
   render() {
-    const { outboundHead } = this.props;
-    const courierNo = outboundHead.courier_no ? outboundHead.courier_no.split(',') : [];
+    const { mailno } = this.state;
+    const courierNo = mailno ? mailno.split(',') : [];
     const dataSource = courierNo.map((item, index) => {
       if (index === 0) {
         return {
@@ -255,20 +346,20 @@ export default class ShunfengExpressModal extends Component {
         onCancel={() => this.props.toggleShunfengExpressModal(false)}
         onOk={() => this.props.toggleShunfengExpressModal(false)}
       >
-        <Tabs defaultActiveKey="1">
+        <Tabs activeKey={this.state.tabkey} onChange={this.handleTabChange}>
           <TabPane tab="订单信息" key="1">
             <Row>
               <Col span={12}>
                 <FormItem label="快递类型" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
                   <Select placeholder="快递类型" value={this.state.express_type} onChange={value => this.setState({ express_type: value })} style={{ width: '100%' }}>
-                    {EXPRESS_TYPES.map(item => (<Option value={item.value}>{item.text}</Option>))}
+                    {EXPRESS_TYPES.map(item => (<Option key={item.value} value={item.value}>{item.text}</Option>))}
                   </Select>
                 </FormItem>
               </Col>
               <Col span={12} >
                 <FormItem label="签单返还" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
                   <RadioGroup value={this.state.need_return_tracking_no}
-                    onChange={value => this.setState({ need_return_tracking_no: value })} style={{ width: '100%' }}
+                    onChange={e => this.setState({ need_return_tracking_no: e.target.value })} style={{ width: '100%' }}
                   >
                     <RadioButton value="0">否</RadioButton>
                     <RadioButton value="1">是</RadioButton>
@@ -280,7 +371,7 @@ export default class ShunfengExpressModal extends Component {
               <Col span={12}>
                 <FormItem label="付款方式" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
                   <Select placeholder="付款方式" value={this.state.pay_method} onChange={value => this.setState({ pay_method: value })} style={{ width: '100%' }}>
-                    {PAY_METHODS.map(item => (<Option value={item.value}>{item.text}</Option>))}
+                    {PAY_METHODS.map(item => (<Option key={item.value} value={item.value}>{item.text}</Option>))}
                   </Select>
                 </FormItem>
               </Col>
@@ -292,22 +383,8 @@ export default class ShunfengExpressModal extends Component {
             </Row>
             <Row>
               <Col span={12}>
-                <FormItem label="增值服务" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-                  <Select placeholder="增值服务" value={this.state.pay_method} onChange={value => this.setState({ pay_method: value })} style={{ width: '100%' }}>
-                    {PAY_METHODS.map(item => (<Option value={item.value}>{item.text}</Option>))}
-                  </Select>
-                </FormItem>
-              </Col>
-              <Col span={12}>
-                <FormItem label="保价金额" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-                  <Input value={this.state.insure} type="number" onChange={e => this.setState({ insure: e.target.value })} />
-                </FormItem>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>
-                <FormItem label="装箱数量" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-                  <Input value={this.state.pack_qty} type="number" onChange={e => this.setState({ pack_qty: e.target.value })} />
+                <FormItem label="包裹数" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                  <Input value={this.state.parcel_quantity} type="number" onChange={e => this.setState({ parcel_quantity: e.target.value })} />
                 </FormItem>
               </Col>
               <Col span={12}>
@@ -328,6 +405,39 @@ export default class ShunfengExpressModal extends Component {
                 </FormItem>
               </Col>
             </Row>
+            <Row>
+              <Col span={12}>
+                <FormItem label="增值服务" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                  <Select placeholder="增值服务" mode="tags"
+                    value={this.state.added_services ? this.state.added_services.split(',') : []}
+                    onChange={value => this.setState({ added_services: value.join(',') })} style={{ width: '100%' }}
+                  >
+                    {ADDED_SERVICES.map(item => (<Option key={item.value} value={item.value}>{item.text}</Option>))}
+                  </Select>
+                </FormItem>
+              </Col>
+              <Col span={12}>
+                {this.state.added_services.indexOf('INSURE') >= 0 &&
+                <FormItem label="保价金额" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                  <Input value={this.state.insure_value} type="number" onChange={e => this.setState({ insure_value: e.target.value })} />
+                </FormItem>
+                }
+              </Col>
+            </Row>
+            {this.state.added_services.indexOf('COD') >= 0 &&
+            <Row>
+              <Col span={12}>
+                <FormItem label="代收货款额" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                  <Input value={this.state.cod_value} onChange={e => this.setState({ cod_value: e.target.value })} />
+                </FormItem>
+              </Col>
+              <Col span={12}>
+                <FormItem label="代收货款卡号" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                  <Input value={this.state.cod_card_id} onChange={e => this.setState({ cod_card_id: e.target.value })} />
+                </FormItem>
+              </Col>
+            </Row>
+            }
             <Card>
               <Row>
                 <Col span={12}>
@@ -392,9 +502,12 @@ export default class ShunfengExpressModal extends Component {
                 </Col>
               </Row>
             </Card>
+            <Button onClick={this.orderExpress}>获取单号</Button>
           </TabPane>
           <TabPane tab="快递单号" key="2">
             <Table dataSource={dataSource} columns={columns} showHeader={false} size="small" pagination={false} />
+            <br />
+            {mailno && <Button type="dashed" icon="plus" style={{ width: '100%' }} onClick={this.handleAddZD}>增加子单号</Button>}
           </TabPane>
         </Tabs>
       </Modal>
