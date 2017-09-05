870a5ec77cb16ad06239467507a57b0479cbf76f
vicki <776128986@qq.com>
2017-09-05 18:45:08 +0800

    modify picklist print

--- client/apps/cwm/shipping/outbound/billsPrint/printPIckList.jsx
+++ client/apps/cwm/shipping/outbound/billsPrint/printPIckList.jsx
@@ -6,8 +6,15 @@ import { Icon } from 'antd';
 import { intlShape, injectIntl } from 'react-intl';
 import messages from '../../message.i18n';
 import { format } from 'client/common/i18n/helpers';
+import JsBarcode from 'jsbarcode';
 
 const formatMsg = format(messages);
+
+function textToBase64Barcode(text) {
+  const canvas = document.createElement('canvas');
+  JsBarcode(canvas, text, { text, fontSize: 24 });
+  return canvas.toDataURL('image/png');
+}
 
 @injectIntl
 @connect(
@@ -27,89 +34,124 @@ export default class Print extends Component {
   msg = key => formatMsg(this.props.intl, key);
   pdfPickHead = () => {
     const { outboundHead, defaultWhse, outboundNo } = this.props;
-    const pdf = [];
-    const header = [];
-    header.push({ text: '拣货单', style: 'tableHeader', colSpan: 6, alignment: 'center' }, {}, {}, {}, {}, {});
-    pdf.push(header);
-    pdf.push([{ text: '入库单号', style: 'table' }, { text: outboundNo, style: 'table' }, { text: '客户订单号', style: 'table' },
-      { text: outboundHead.cust_order_no, style: 'table' }, { text: '出库日期', style: 'table' }, { text: '', style: 'table' }]);
-    pdf.push([{ text: '货物属性', style: 'table' }, { text: outboundHead.bonded ? '保税' : '非保税', style: 'table' }, { text: '客户', style: 'table' },
-      { text: outboundHead.owner_name, style: 'table' }, { text: '仓库', style: 'table' }, { text: defaultWhse.name, style: 'table' }]);
-    pdf.push([{ text: '备注', style: 'table' }, { text: '', colSpan: 5 }, {}, {}, {}, {}]);
-    return pdf;
+    const barcode = textToBase64Barcode(outboundNo);
+    const headContent = [
+      { columns: [
+        { text: '拣货单', style: 'title', alignment: 'center' },
+      ] },
+      { columns: [
+        { text: '' },
+        { image: barcode, width: 200 },
+        { text: '' },
+      ] },
+      { columns: [
+        { text: `入库单号:  ${outboundNo || ''}`, style: 'header' },
+        { text: `客户订单号:  ${outboundHead.cust_order_no || ''}`, style: 'header' },
+        { text: '出库日期:  ', style: 'header' },
+      ] },
+      { columns: [
+        { text: `货物属性:  ${outboundHead.bonded ? '保税' : '非保税'}`, style: 'header' },
+        { text: `客户:  ${outboundHead.owner_name || ''}`, style: 'header' },
+        { text: `仓库:  ${defaultWhse.name || ''}`, style: 'header' },
+      ] },
+      { columns: [
+        { text: '备注: ', style: 'header' },
+      ] },
+    ];
+    return headContent;
   }
   pdfPickDetails = () => {
     const { outboundHead, pickDetails } = this.props;
+    let datas = [];
+    if (pickDetails.length !== 16) {
+      datas = pickDetails.concat([{}]);
+    }
     const pdf = [];
-    const header = [];
-    header.push({ text: '货物清单', style: 'tableHeader', colSpan: 7, alignment: 'center' }, {}, {}, {}, {}, {}, {});
-    pdf.push(header);
-    pdf.push([{ text: '项', style: 'table', alignment: 'center' }, { text: '货号', style: 'table', alignment: 'center' },
-      { text: '产品名称', style: 'table', alignment: 'center' }, { text: '待捡数', style: 'table', alignment: 'center' },
-      { text: '实件数', style: 'table', alignment: 'center' }, { text: '库位', style: 'table', alignment: 'center' },
-      { text: '扩展属性1', style: 'table', alignment: 'center' }]);
-    for (let i = 0; i < pickDetails.length; i++) {
-      pdf.push([i + 1, pickDetails[i].product_no, pickDetails[i].name, pickDetails[i].alloc_qty,
-        '', pickDetails[i].location, '']);
+    pdf.push([{ text: '项', style: 'tableHeader', alignment: 'center' }, { text: '货号', style: 'tableHeader', alignment: 'center' },
+      { text: '产品名称', style: 'tableHeader', alignment: 'center' }, { text: '待捡数', style: 'tableHeader', alignment: 'center' },
+      { text: '实件数', style: 'tableHeader', alignment: 'center' }, { text: '库位', style: 'tableHeader', alignment: 'center' },
+      { text: '扩展属性1', style: 'tableHeader', alignment: 'center' }]);
+    for (let i = 0; i < datas.length; i++) {
+      const data = datas[i];
+      pdf.push([i + 1, data.product_no || '', data.name || '', data.alloc_qty || '', '', data.location || '', '']);
     }
     pdf.push(['合计', '', '', outboundHead.total_alloc_qty, '', '', '']);
     return pdf;
   }
   pdfSign = () => {
-    const pdf = [];
-    pdf.push([{ text: '签字', style: 'tableHeader', colSpan: 8, alignment: 'center' }, {}, {}, {}, {}, {}, {}, {}]);
-    pdf.push([{ text: '计划', style: 'table' }, '', { text: '收货', style: 'table' }, '', { text: '上架', style: 'table' },
-      '', { text: '归档', style: 'table' }, '']);
-    pdf.push([{ text: '实收包装件数', style: 'table' }, { text: '', colSpan: 2 }, {}, { text: '实测计价体积', style: 'table' },
-      { text: '', colSpan: 2 }, {}, { text: '记录人', style: 'table' }, '']);
-    return pdf;
+    const foot = [
+      { columns: [
+        { text: moment(new Date()).format('YYYY/MM/DD'), fontSize: 9, alignment: 'right' },
+      ] },
+      { columns: [
+        { text: '计划:', fontSize: 11 },
+        { text: '收货:', fontSize: 11 },
+        { text: '上架:', fontSize: 11 },
+        { text: '归档:', fontSize: 11 },
+      ] },
+    ];
+    return foot;
   }
   handleDocDef = () => {
+    const { pickDetails } = this.props;
     const docDefinition = {
       content: [],
+      pageOrientation: 'landscape',
+      pageSize: 'A4',
       styles: {
-        eachheader: {
-          fontSize: 9,
-          margin: [40, 20, 30, 30],
+        title: {
+          fontSize: 18,
+          bold: true,
+          alignment: 'center',
+          width: '100%',
+          margin: [0, 0, 0, 8],
+        },
+        header: {
+          fontSize: 10,
+          margin: [0, 3, 0, 4],
         },
         table: {
-          fontSize: 9,
-          color: 'black',
-          margin: [2, 10, 2, 10],
-        },
-        tableHeader: {
-          fontSize: 12,
-          bold: true,
+          fontSize: 10,
           color: 'black',
           margin: [2, 2, 2, 2],
+        },
+        tableHeader: {
+          fontSize: 11,
+          bold: true,
+          color: 'black',
+          margin: [2, 5, 2, 5],
+        },
+        footer: {
+          fontSize: 8,
         },
       },
       defaultStyle: {
         font: 'yahei',
       },
     };
-    docDefinition.header = {
-      columns: [
-        { text: moment(new Date()).format('YYYY/MM/DD'), style: 'eachheader' },
-      ],
-    };
+    let num = 0;
+    if (pickDetails.length > 16) {
+      num = 24 - (pickDetails.length - 16) % 24;
+    } else {
+      num = 16 - pickDetails.length;
+    }
     docDefinition.content = [
-      { style: 'table',
-        table: { widths: [60, 150, 60, 75, 50, 75], headerRows: 1, body: this.pdfPickHead() },
-      },
+      this.pdfPickHead(),
       {
         style: 'table',
-        table: { widths: [40, 80, 140, 50, 50, 50, 50], headerRows: 1, body: this.pdfPickDetails() },
+        table: { widths: ['3%', '22%', '27%', '12%', '12%', '12%', '12%'], headerRows: 1, body: this.pdfPickDetails() },
+        layout: {
+          hLineColor: 'gray',
+          vLineColor: 'gray',
+          paddingBottom(i, node) { console.log(node.table.body, num); return (node.table.body[i][3].text === '') ? 10 * num : 1; },
+        },
       },
-      {
-        style: 'table',
-        table: { widths: [40, 75, 40, 75, 40, 75, 40, 75], headerRows: 1, body: this.pdfSign() },
-      },
+      this.pdfSign(),
     ];
+    docDefinition.footer = (currentPage, pageCount) => ({ text: `第 ${currentPage.toString()}页，共 ${pageCount}页`, alignment: 'center', style: 'footer' });
     return docDefinition;
   }
   handlePrint = () => {
-    // pdfMake.vfs = pdfFonts.pdfMake.vfs;
     const docDefinition = this.handleDocDef();
     window.pdfMake.fonts = {
       yahei: {
