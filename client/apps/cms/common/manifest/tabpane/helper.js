41ad3e923db06d91648558d5ae4ce8611dd20848
kayw <kayw@outlook.com>
2017-09-12 20:07:08 +0800

    dividGrossWt max decimal

--- client/apps/cms/common/manifest/tabpane/helper.js
+++ client/apps/cms/common/manifest/tabpane/helper.js
@@ -16,17 +16,24 @@ export function dividGrossWt(netWts, totalGrossWt) {
   let totalRoundGrossWt = 0;
   let maxNetWtIdx = 0;
   const decimal = countDecimal(totalGrossWt);
+  const netwtdecimal = countDecimal(netWtSum);
   const fixed = decimal < 2 ? 2 : decimal;
+  let finalFixed = fixed;
   for (let i = 0; i < netWts.length; i++) {
     const netwt = netWts[i];
-    const grosswt = Number((totalGrossWt * netwt / netWtSum).toFixed(fixed));
+    const divgrosswt = totalGrossWt * netwt / netWtSum;
+    let grosswt = Number(divgrosswt.toFixed(fixed));
+    if (grosswt === 0) {
+      grosswt = Number(divgrosswt.toFixed(netwtdecimal));
+      finalFixed = netwtdecimal;
+    }
     totalRoundGrossWt += grosswt;
     grossWts.push(grosswt);
     if (netwt > netWts[maxNetWtIdx]) {
       maxNetWtIdx = i;
     }
   }
-  grossWts[maxNetWtIdx] = Number((grossWts[maxNetWtIdx] + totalGrossWt - totalRoundGrossWt).toFixed(fixed));
+  grossWts[maxNetWtIdx] = Number((grossWts[maxNetWtIdx] + totalGrossWt - totalRoundGrossWt).toFixed(finalFixed));
   return grossWts;
 }
 
@@ -39,16 +46,23 @@ export function dividAmount(tradeAmounts, totalAmount) {
   let totalRoundAmount = 0;
   let maxAmountIdx = 0;
   const decimal = countDecimal(totalAmount);
+  const tradedecimal = countDecimal(tradeTotal);
   const fixed = decimal < 2 ? 2 : decimal;
+  let finalFixed = fixed;
   for (let i = 0; i < tradeAmounts.length; i++) {
     const tradeamt = tradeAmounts[i];
-    const amt = Number((totalAmount * tradeamt / tradeTotal).toFixed(fixed));
+    const divamt = (totalAmount * tradeamt / tradeTotal);
+    let amt = Number(divamt.toFixed(fixed));
+    if (amt === 0) {
+      amt = Number(divamt.toFixed(tradedecimal));
+      finalFixed = tradedecimal;
+    }
     totalRoundAmount += amt;
     amts.push(amt);
     if (tradeamt > tradeAmounts[maxAmountIdx]) {
       maxAmountIdx = i;
     }
   }
-  amts[maxAmountIdx] = Number((amts[maxAmountIdx] + totalAmount - totalRoundAmount).toFixed(fixed));
+  amts[maxAmountIdx] = Number((amts[maxAmountIdx] + totalAmount - totalRoundAmount).toFixed(finalFixed));
   return amts;
 }
