6596bfa98416a0f7d47f61236b60345969a16d17
zhaochenchen <ialwayswannafly@yahoo.com>
2017-11-02 13:55:40 +0800

    fixed param tenantId

--- client/apps/cwm/stock/movement/modal/movementModal.jsx
+++ client/apps/cwm/stock/movement/modal/movementModal.jsx
@@ -249,7 +249,7 @@ export default class MovementModal extends Component {
     });
   }
   handleCreateMovement = () => {
-    this.props.createMovement(this.state.owner.id, this.state.owner.name, this.state.moveType, '', this.props.defaultWhse.code, this.props.tenantId,
+    this.props.createMovement(this.state.owner.id, this.state.owner.name, this.state.moveType, '', this.props.defaultWhse.code,
       this.props.loginName, this.state.movements).then((result) => {
         if (!result.err) {
           this.props.closeMovementModal();
