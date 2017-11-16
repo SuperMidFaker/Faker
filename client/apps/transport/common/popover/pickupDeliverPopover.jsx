af820bcf7e99b2f0934d954435b1d7bccc9c04de
kayw <kayw@outlook.com>
2017-11-16 15:21:38 +0800

    importComparison uuid use nextProps

--- client/apps/transport/common/popover/pickupDeliverPopover.jsx
+++ client/apps/transport/common/popover/pickupDeliverPopover.jsx
@@ -44,20 +44,6 @@ export default class PickupDeliverPopover extends React.Component {
     visible: false,
     warningMessage: '',
   }
-    /*
-  componentDidMount() {
-    const { shipmtNo } = this.props;
-    window.$(document).click((event) => {
-      console.log(event.target);
-      const pickupDeliverClicked = window.$(event.target).closest(`.pickupDeliver${shipmtNo}`).length > 0;
-      const antPopoverClicked = window.$(event.target).closest('.ant-popover').length > 0;
-      const calenderClicked = window.$(event.target).closest('.ant-calendar-picker-container').length > 0;
-      console.log('pickup', pickupDeliverClicked, 'antpopover', antPopoverClicked, 'calendar', calenderClicked, 'visible', this.state.visible);
-      if (!pickupDeliverClicked && !calenderClicked && !antPopoverClicked && this.state.visible) {
-        this.handleClose();
-      }
-    });
-  } */
   msg = descriptor => formatMsg(this.props.intl, descriptor)
   handleOk = () => {
     this.props.form.validateFields((errors) => {
