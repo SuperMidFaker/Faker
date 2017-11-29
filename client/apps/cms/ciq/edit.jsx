5c0ee822d53f8b8171eea2e3a2b38256af086a6b
zhaochenchen <ialwayswannafly@yahoo.com>
2017-11-29 14:55:42 +0800

    modified ciqList

--- client/apps/cms/ciq/edit.jsx
+++ client/apps/cms/ciq/edit.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Badge, Form, Breadcrumb, Button, Icon, Layout, Tabs, message, Dropdown, Menu } from 'antd';
+import { Form, Breadcrumb, Button, Icon, Layout, Tabs, Dropdown, Menu } from 'antd';
 import { intlShape, injectIntl } from 'react-intl';
 import connectNav from 'client/common/decorators/connect-nav';
-import { setNavTitle } from 'common/reducers/navbar';
-import { loadEntry, saveEntryHead } from 'common/reducers/cmsManifest';
-import { deleteDecl, setDeclReviewed, openDeclReleasedModal, showSendDeclModal } from 'common/reducers/cmsDeclare';
 import PageHeader from 'client/components/PageHeader';
 import MagicCard from 'client/components/MagicCard';
 import CiqDeclHeadPane from './tabpane/ciqDeclHeadPane';
 import CiqDeclGoodsPane from './tabpane/ciqDeclGoodsPane';
-// import ContainersPane from './tabpane/containersPane';
-// import AttachedDocsPane from './tabpane/attachedDocsPane';
-// import AttachedCertsPane from './tabpane/attachedCertsPane';
 import { format } from 'client/common/i18n/helpers';
 import messages from './message.i18n';
-import { CMS_DECL_STATUS } from 'common/constants';
-import { showPreviewer } from 'common/reducers/cmsDelgInfoHub';
 
 const formatMsg = format(messages);
 const { Content } = Layout;
@@ -32,13 +24,10 @@ const navObj = {
 @injectIntl
 @connect(
   state => ({
-    billMeta: state.cmsManifest.billMeta,
-    head: state.cmsManifest.entryHead,
-    bodies: state.cmsManifest.entryBodies,
     tenantId: state.account.tenantId,
-    formRequire: state.cmsManifest.params,
+    ciqDeclHead: state.cmsCiqDeclare.ciqDeclHead,
   }),
-  { saveEntryHead, loadEntry, deleteDecl, setDeclReviewed, openDeclReleasedModal, showSendDeclModal, setNavTitle, showPreviewer }
+  { }
 )
 @connectNav(navObj)
 @Form.create()
@@ -46,77 +35,36 @@ export default class CiqDeclEdit extends React.Component {
   static propTypes = {
     intl: intlShape.isRequired,
     ioType: PropTypes.oneOf(['in', 'out']),
-    billMeta: PropTypes.shape({
-      bill_seq_no: PropTypes.string.isRequired,
-      entries: PropTypes.arrayOf(PropTypes.shape({ pre_entry_seq_no: PropTypes.string })),
-    }),
-    declSpinning: PropTypes.bool.isRequired,
   }
   static contextTypes = {
     router: PropTypes.object.isRequired,
   }
   state = {
-    visible: false,
     collapsed: true,
     fullscreen: true,
   }
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.params.preEntrySeqNo !== this.props.params.preEntrySeqNo) {
-      this.props.loadEntry(nextProps.params.billseqno, nextProps.params.preEntrySeqNo, nextProps.tenantId);
-      this.props.setNavTitle(navObj);
-    }
-  }
   msg = (descriptor, values) => formatMsg(this.props.intl, descriptor, values)
-  toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  }
   toggleFullscreen = (fullscreen) => {
     this.setState({ fullscreen });
   }
-  handleDock = () => {
-    this.setState({ visible: true });
-  }
   handleManifestVisit = () => {
-    const { ioType, billMeta } = this.props;
-    const pathname = `/clearance/${ioType}/manifest/view/${billMeta.bill_seq_no}`;
+    const { ciqDeclHead } = this.props;
+    const pathname = `/clearance/${this.context.router.params.ioType === 'in' ? 'import' : 'export'}/manifest/view/${ciqDeclHead.bill_seq_no}`;
     this.context.router.push({ pathname });
   }
-  handleDelete = () => {
-    const head = this.props.head;
-    this.props.deleteDecl(head.id, head.bill_seq_no).then((result) => {
-      if (result.error) {
-        message.error(result.error.message, 10);
-      } else {
-        this.context.router.push(`/clearance/${this.props.ioType}/customs`);
-      }
-    });
-  }
-
-  reloadEntry = () => {
-    this.props.loadEntry(this.props.head.bill_seq_no, this.props.head.pre_entry_seq_no, this.props.tenantId);
-  }
-  handlePreview = (delgNo) => {
-    this.props.showPreviewer(delgNo, 'customsDecl');
-  }
   render() {
-    const { form, head, bodies, billMeta } = this.props;
-    const declkey = Object.keys(CMS_DECL_STATUS).filter(stkey => CMS_DECL_STATUS[stkey].value === head.status)[0];
     const declEntryMenu = (
-      <Menu onClick={this.handleLinkMenuClick}>
-        {billMeta.entries.map(bme => (<Menu.Item key={bme.pre_entry_seq_no}>
-          <Icon type="file" /> 关联报关单{bme.entry_id || bme.pre_entry_seq_no}</Menu.Item>)
-        )}
+      <Menu onClick={this.handleManifestVisit}>
+        <Menu.Item><Icon type="file" />申报清单</Menu.Item>
       </Menu>);
     const tabs = [];
     tabs.push(
       <TabPane tab="基本信息" key="header">
-        <CiqDeclHeadPane ioType={this.props.params.ioType} form={form} formData={head} />
+        <CiqDeclHeadPane ioType={this.props.params.ioType} />
       </TabPane>);
     tabs.push(
       <TabPane tab="商品信息" key="body">
-        <CiqDeclGoodsPane ioType={this.props.params.ioType} data={bodies} headNo={head.id} fullscreen={this.state.fullscreen} />
+        <CiqDeclGoodsPane ioType={this.props.params.ioType} fullscreen={this.state.fullscreen} />
       </TabPane>);
     return (
       <Layout>
@@ -127,16 +75,10 @@ export default class CiqDeclEdit extends React.Component {
                 {this.msg('ciqDecl')}
               </Breadcrumb.Item>
               <Breadcrumb.Item>
-                <a onClick={() => this.handlePreview(head.delg_no)}>{head.bill_seq_no}</a>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item>
-                {head.entry_id || head.pre_entry_seq_no}
+                {this.props.router.params.declNo}
               </Breadcrumb.Item>
             </Breadcrumb>
           </PageHeader.Title>
-          <PageHeader.Nav>
-            {declkey && <Badge status={CMS_DECL_STATUS[declkey].badge} text={CMS_DECL_STATUS[declkey].text} />}
-          </PageHeader.Nav>
           <PageHeader.Actions>
             <Dropdown overlay={declEntryMenu}>
               <Button ><Icon type="link" />转至 <Icon type="down" /></Button>
