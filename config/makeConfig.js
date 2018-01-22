0de968acb4ea9d635e5cc6987a7378fb28043c8b
Eric Shao <eric.shao@welogix.cn>
2018-01-22 13:07:25 +0800

    Changed staging CDN to st-cdn.welogix.co

--- config/makeConfig.js
+++ config/makeConfig.js
@@ -84,7 +84,7 @@ module.exports = (serverPort, dirName, appName) => {
       notify: 'http://notify.welogix.co/',
       self: '/',
     });
-    config.set('CDN_URL', 'http://staging-cdn.welogix.co');
+    config.set('CDN_URL', 'http://st-cdn.welogix.co');
     config.set('webpack_public_path', `${config.get('CDN_URL')}/${config.get('webpack_dist')}/`);
     // config.set('webpack_public_path', `/${config.get('webpack_dist')}/`);
   }
