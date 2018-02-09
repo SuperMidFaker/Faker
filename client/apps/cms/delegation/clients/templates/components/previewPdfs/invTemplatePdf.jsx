7bd7804a3d4d0d6b0fa0d26ea4051861811502da
vicki <776128986@qq.com>
2018-02-09 18:19:57 +0800

    preview invoice pdf

--- client/apps/cms/delegation/clients/templates/components/previewPdfs/invTemplatePdf.jsx
+++ client/apps/cms/delegation/clients/templates/components/previewPdfs/invTemplatePdf.jsx
@@ -1,21 +1,70 @@
+import moment from 'moment';
+import { CMS_DOCU_TYPE } from 'common/constants';
+
+  function getBase64FromImageUrl(url) {
+    const img = new Image();
+    img.src = url;
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    const dataURL = canvas.toDataURL("image/png");
+    return dataURL;
+  }
+
+  function headBody(docu, trxText) {
+    const body = [
+      [{ stack: [
+          { text: `Consignee :`, style: 'subheader' },
+          { text: `${docu.consignee || ''}`, style: 'headContent' }
+        ]},
+      { stack: [
+          { text: '买方 Buyer :', style: 'subheader' },
+          { text: `${docu.buyer || ''}` , style: 'headContent'}
+        ],
+      }],
+      [{stack: [
+        { text: `Terms Of Payment :`, style: 'subheader' },
+        { text: `${docu.payment_terms || ''}`, style: 'headContent' },
+        { text: 'Terms Of Delivery :', style: 'subheader' },
+        { text: `${trxText}` , style: 'headContent'},
+        { text: 'Insurance :', style: 'subheader' },
+        { text: `${docu.insurance || ''}` , style: 'headContent'}
+      ]},{
+        stack: [
+          { text: 'Notify contacts', style: 'subheader' },
+          { text: `${docu.notify || ''}` , style: 'headContent'}
+        ]
+      }]
+    ];
+    if (docu.smarks_en) {
+      body.push([{ text: `Shipping Marks\n ${docu.shipping_marks || ''}`, style: 'subheader', colSpan: 2, },'']);
+    }
+    return body;
+  }
+
   function pdfBody(docu, docuBody) {
     const pdf = [];
     const header = [];
+    let widths = ['8%', '10%', '20%'];
     header.push({ text: '序号', style: 'tableHeader' }, { text: '货号', style: 'tableHeader' }, { text: '中文品名', style: 'tableHeader' });
     if (docu.eng_name_en) {
       header.push({ text: '英文品名', style: 'tableHeader' });
+      widths.push('*');
     }
     header.push(
       { text: '原产国', style: 'tableHeader' },
       { text: '数量', style: 'tableHeader' },
     );
+    widths.push('10%', '10%');
     if (docu.unit_price_en) {
       header.push({ text: '单价', style: 'tableHeader' });
+      widths.push('*');
     }
     header.push(
       { text: '金额', style: 'tableHeader' },
       { text: '净重', style: 'tableHeader' },
     );
+    widths.push('*', '*');
     pdf.push(header);
     for (let i = 0; i < docuBody.length; i++) {
       const dbody = docuBody[i];
@@ -59,91 +108,134 @@
       footer.push({ text: `${(sumval.wet_wt).toFixed(3)}`, alignment: 'right' });
       pdf.push(footer);
     }
-    return pdf;
+    const bodytable = { headerRows: 1, widths, body: pdf };
+    return bodytable;
   }
   export function invTempPdfDef (docu, trxModes, docuBody) {
     const trxmode = trxModes.find(trx => trx.key === docu.trxn_mode);
     const trxText = trxmode ? trxmode.text : '';
     const docDefinition = {
       content: [],
+      pageMargins: [40, 120, 40, 120],
       styles: {
-        eachheader: {
-          fontSize: 9,
-          margin: [40, 20, 30, 30],
-        },
         header: {
-          fontSize: 14,
+          fontSize: 13,
           bold: true,
-          margin: [0, 0, 0, 10],
+          alignment: 'left',
+          margin: [60, 20, 2, 10],
+        },
+        subtitle: {
+          fontSize: 10,
+          bold: true,
+          color: 'black',
+          margin: [60, 4, 2, 4],
+        },
+        subheader: {
+          fontSize: 10,
+          bold: true,
+          color: 'black',
+          margin: [4, 2, 2, 2],
+        },
+        headContent: {
+          fontSize: 9,
+          bold: false,
+          margin: [8, 2, 2, 2],
         },
         table: {
           fontSize: 10,
           color: 'black',
-          margin: [2, 10, 2, 10],
+          margin: [2, 5, 2, 5],
         },
         tableHeader: {
-          fontSize: 12,
-          bold: true,
-          color: 'black',
-          margin: [2, 2, 2, 2],
+          fontSize: 11,
+          bold: false,
+          margin: [2, 5, 2, 5],
         },
-        footer: {
-          margin: [2, 12, 2, 12],
+        bottom: {
+          fontSize: 10,
+          bold: false,
+          margin: [10, 10, 2, 10],
         },
-        sign: {
-          fontSize: 12,
-          bold: true,
-          color: 'black',
-          margin: [12, 12, 12, 12],
+        footTable: {
+          fontSize: 8,
+          bold: false,
+          margin: [40, 40],
         },
       },
       defaultStyle: {
         font: 'yahei',
+        columnGap: 20,
       },
     };
-    if (docu.docu_type === CMS_DOCU_TYPE.invoice) {
+    let logoUrl = 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png';
+    const logoFilter = docu.imgs.filter(img => img.img_type === 0)[0];
+    if (logoFilter) {
+      logoUrl = logoFilter.url;
+    }
+    const logo = getBase64FromImageUrl(logoUrl);
+    // console.log('logo', logo);
+    // if (docu.docu_type === CMS_DOCU_TYPE.invoice) {
+      let hbodyHeight = ["*", "*"];
+      if (docu.smarks_en) {
+        hbodyHeight = ["*", "*", 70];
+      }
       docDefinition.header = {
+        // margin: 10,
         columns: [
-          'logo',
-          [
-            { text: 'Invoice', style: 'header'},
-            {
-              columns: [
-                { text: `${docu.subtitle}`, style: 'eachheader' },
-                { text: `Invoice No :\n ${docu.docu_no} \nInvoice Date :\n ${moment(docu.date).format('YYYY.MM.DD')}`, style: 'eachheader' },
+          {
+            image: logo,
+            width: 120,
+            margin: [20, 0, 20, 20],
+          },
+          [ { text: 'Invoice', style: 'header'},
+            { columns: [
+                { text: `${docu.subtitle || ''}`, style: 'subtitle' },
+                { stack: [
+                  { text: 'Invoice No :', style: 'subheader'},
+                  { text: `${docu.docu_no || ''}`, style: 'headContent'},
+                  { text: 'Invoice Date :', style: 'subheader'},
+                  { text: `${moment(docu.date).format('YYYY.MM.DD') || ''}`, style: 'headContent'},
+                ]},
               ]
             }
-          ]
-        ],
+          ],
+        ]
       };
       docDefinition.content = [
-        { columns: [
-            { text: `Consignee :  ${docu.consignee || ''}` },
-            { text: `买方 Buyer :  ${docu.buyer || ''}` },
-          ]
-        },
-        { columns: [
-            { text: `Terms Of Payment :\n ${docu.payment_terms || ''}\nTerms Of Delivery :\n ${trxText}\nInsurance :\n ${docu.insurance || ''}` },
-            { text: `Notify contacts\n ${docu.notify || ''}` },
-          ]
+        {
+          style: 'table',
+          table: {
+            widths: '50%',
+            heights: hbodyHeight,
+            body: headBody(docu, trxText),
+          }
         },
       ];
-      if (docu.smarks_en) {
-        docDefinition.content.push({ text: `Shipping Marks\n ${docu.shipping_marks || ''}` });
-      }
       docDefinition.content.push({
         style: 'table',
-        table: { headerRows: 1, body: pdfBody(docu, docuBody) },
+        table: pdfBody(docu, docuBody),
       });
       if (docu.packages_en) {
-        docDefinition.content.push({ text: `Number Of Packages:  ${docu.packages || ''}` });
+        docDefinition.content.push({ text: `Number Of Packages:  ${docu.packages || ''}`, style: 'bottom' });
       }
       if (docu.gross_wt_en) {
-        docDefinition.content.push({ text: `Gross Weight: ${docu.gross_wt || ''} Kgs` });
+        docDefinition.content.push({ text: `Gross Weight: ${docu.gross_wt || ''} Kgs`, style: 'bottom' });
       }
       if (docu.remark_en) {
-        docDefinition.content.push({ text: `Remark :  ${docu.remark || ''}` });
+        docDefinition.content.push({ text: `Remark :  \n${docu.remark || ''}`, style: 'bottom' });
       }
-    }
+      docDefinition.footer = {
+        style: 'footTable',
+        table: { 
+          heights: [60],
+          widths: '33%',
+          body: [[
+            { text: `${docu.footer1 || ''}`, border: [false, true, false, false] },
+            { text: `${docu.footer2 || ''}`, border: [false, true, false, false] },
+            { text: `${docu.footer3 || ''}`, border: [false, true, false, false]}
+          ]]
+        },
+      };
+    // }
     return docDefinition;
   }
