1eaf5f6cef5f6b53af703ff5ab2268c50b61a246
vicki <776128986@qq.com>
2018-02-28 18:07:47 +0800

    modify import/export stats

--- client/apps/cms/dashboard/card/exportStatsCard.jsx
+++ client/apps/cms/dashboard/card/exportStatsCard.jsx
@@ -1,35 +1,59 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import { intlShape, injectIntl } from 'react-intl';
 import { Dropdown, Icon, Menu } from 'antd';
-import { MiniBar, yuan, Field } from 'client/components/Charts';
+import { MiniBar, Field } from 'client/components/Charts';
 import ChartCard from 'client/components/ChartCard';
-import { loadStatsCard } from 'common/reducers/cwmDashboard';
 import { format } from 'client/common/i18n/helpers';
 import messages from '../message.i18n';
 
 const formatMsg = format(messages);
 
 @injectIntl
-@connect(
-  state => ({
-    statsCard: state.cwmDashboard.statsCard,
-  }),
-  { loadStatsCard }
-)
+@connect(state => ({
+  exportStats: state.cmsDashboard.exportStats,
+}))
 
 export default class ExportStatsCard extends Component {
   static propTypes = {
     intl: intlShape.isRequired,
+    exportStats: PropTypes.object.isRequired,
+  }
+  state = {
+    amount: 0,
+    count: 0,
+    currency: 'CNY',
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.exportStats !== this.props.exportStats) {
+      if (this.state.currency === 'USD') {
+        this.setState({
+          amount: nextProps.exportStats.total_usd,
+          count: nextProps.exportStats.count,
+        });
+      } else if (this.state.currency === 'CNY') {
+        this.setState({
+          amount: nextProps.exportStats.total_cny,
+          count: nextProps.exportStats.count,
+        });
+      }
+    }
   }
   msg = key => formatMsg(this.props.intl, key);
+  handleMenuClick = (e) => {
+    if (e.key === 'USD') {
+      this.setState({ currency: e.key, amount: this.props.exportStats.total_usd });
+    } else if (e.key === 'CNY') {
+      this.setState({ currency: e.key, amount: this.props.exportStats.total_cny });
+    }
+  }
   render() {
-    // const { statsCard } = this.props;
-    const menu = (<Menu>
-      <Menu.Item>USD</Menu.Item>
-      <Menu.Item>CNY</Menu.Item>
+    const { amount, count, currency } = this.state;
+    const menu = (<Menu key={currency} onClick={this.handleMenuClick}>
+      <Menu.Item key="USD">USD</Menu.Item>
+      <Menu.Item key="CNY">CNY</Menu.Item>
     </Menu>);
     const mockData = [];
     const beginDay = (new Date().getTime()) - (1000 * 60 * 60 * 24 * 30);
@@ -42,10 +66,10 @@ export default class ExportStatsCard extends Component {
     return (
       <ChartCard
         bordered={false}
-        title="出口金额"
+        title="进口金额"
         action={<Dropdown overlay={menu}><Icon type="ellipsis" /></Dropdown>}
-        total={yuan(6560)}
-        footer={<Field label="出口量" value="600" />}
+        total={`${currency}` === 'CNY' ? `￥${amount}` : `$${amount}`}
+        footer={<Field label="进口量" value={count} />}
         contentHeight={64}
       >
         <MiniBar
