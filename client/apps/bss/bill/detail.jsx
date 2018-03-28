5a68a58a6d5e9d8a00f9b826486a087dd942b35a
zhaochenchen <ialwayswannafly@yahoo.com>
2018-03-28 10:13:32 +0800

    fixed billDetails

--- client/apps/bss/bill/detail.jsx
+++ client/apps/bss/bill/detail.jsx
@@ -23,7 +23,7 @@ const { TabPane } = Tabs;
   state => ({
     tenantId: state.account.tenantId,
     billHead: state.bssBill.billHead,
-    billHeadReload: state.bssBill.billHeadReload,
+    reload: state.bssBill.reload,
   }),
   { loadBillHead }
 )
@@ -43,7 +43,7 @@ export default class ReceivableBillDetail extends Component {
     this.props.loadBillHead(this.props.params.billNo);
   }
   componentWillReceiveProps(nextProps) {
-    if (nextProps.billHeadReload) {
+    if (nextProps.reload) {
       this.props.loadBillHead(this.props.params.billNo);
     }
   }
